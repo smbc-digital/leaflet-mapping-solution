90ea40bfa7a5c1c0a4d993b384ce396724100450
stephen.lally <stephen.lally.smbc@gmail.com>
2021-04-26 13:41:20 +0100

    Revert "test(project): delete map config"
    This reverts commit 928c158c09365d7b8922aa67331c051c100d00ae.

---
+++ Configuration/flooding/index.js
@@ -0,0 +1,9 @@
+export default {
+    Map: {
+        StartingLatLng: [50.3915, -3.125143],
+        StartingZoom: 2,
+        DisplayGrayScale: true,
+        DisplayStreets: true,
+        Class: 'govuk-grid-column-full smbc-map__container'
+    }
+}
