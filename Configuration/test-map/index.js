4540dc5d0280bf2a92d703f7cc865718e96a32bd
Lydia Gray <lydia.gray@stockport.gov.uk>
2021-04-27 11:22:25 +0100

    test(config): remove config folder for testing

--- Configuration/test-map/index.js
+++
@@ -1,9 +0,0 @@
-export default {
-    Map: {
-        StartingLatLng: [53.3915, -2.125143],
-        StartingZoom: 2,
-        DisplayGrayScale: true,
-        DisplayStreets: true,
-        Class: 'govuk-grid-column-full smbc-map__container'
-    }
-}
