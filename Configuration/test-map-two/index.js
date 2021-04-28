388beb1e9e1dd80bd0c60166ceb6c24b6b76afa3
michaellau-smbc <michael.lau@stockport.gov.uk>
2021-04-28 15:46:59 +0100

    chore(controls): add zoom, locate, layers, address and fullscreen feature

--- Configuration/test-map-two/index.js
+++ Configuration/test-map-two/index.js
@@ -2,8 +2,7 @@ export default {
     Map: {
         StartingLatLng: [41.3915, -2.125143],
         StartingZoom: 2,
-        DisplayGrayScale: true,
-        DisplayStreets: true,
+        EnableLocateControl: true,
         Class: 'govuk-grid-column-full smbc-map__container'
     }
 }
