4d9db8d4f452baed660038d30a921dbfb2c9ee32
michaellau-smbc <michael.lau@stockport.gov.uk>
2021-04-28 16:48:36 +0100

    refactor(config) remove displayGrayScale and DisplayStreets

--- Configuration/test-map/index.js
+++ Configuration/test-map/index.js
@@ -2,8 +2,7 @@ export default {
     Map: {
         StartingLatLng: [53.3915, -2.125143],
         StartingZoom: 2,
-        DisplayGrayScale: true,
-        DisplayStreets: true,
+        EnableLocateControl: false,
         Class: 'govuk-grid-column-full smbc-map__container'
     }
 } 
