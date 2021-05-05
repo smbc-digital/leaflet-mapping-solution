6aad3a286a52bf21a4bae9945f208858a09c0e0e
Lydia Gray <lydia.gray@stockport.gov.uk>
2021-05-05 11:38:52 +0100

    fix(configurations): fix configs to work with new structure

--- Configuration/map-with-dynamic-data/index.js
+++ Configuration/map-with-dynamic-data/index.js
@@ -2,49 +2,28 @@ import Leaflet from 'leaflet'
 
 export default {
     Map: {
-        StartingLatLng: [53.3915, -2.125143],
         StartingZoom: 2,
-        EnableLocateControl: true,
-        Class: 'govuk-grid-column-full smbc-map__container'
+        DisplayBoundary: false
     },
     Tiles: {
         Token: 'pk.eyJ1IjoiZ2lzLXN0b2NrcG9ydCIsImEiOiJja2xxcTUzZGUxZml1MnFxeTNidmt5c3kyIn0.QOsaZ7uPj4Kv2lwL63Crlg'
     },
-    DynamicData:
-        [
-            {
-                key: 'Street Lights',
-                url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=highway_assets:street_lights_reporting&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
-                layerOptions: {
-                    maxZoom: 16,
-                    onEachFeature: () => { },
-                    pointToLayer: (feature, latlng) => {
-                        return Leaflet.circleMarker(latlng, {
-                            color: '#000',
-                            weight: 2,
-                            opacity: 1,
-                            fillColor: '#33a02c',
-                            fillOpacity: 0.5
-                        })
-
-                    }
-                },
-                displayOverlay: true,
-                visibleByDefault: true
-            },
-
-            {
-                key: 'os1250_line',
-                url: 'http://spatial.stockport.gov.uk/geoserver/wms?',
-                layerOptions: {
-                    maxZoom: 20,
-                    minZoom: 18,
-                    layers: 'base_maps:os1250_line',
-                    format: 'image/png',
-                    transparent: true
-                },
-                displayOverlay: false,
-                visibleByDefault: true
+    DynamicData: [
+        {
+            key: 'Street Lights',
+            typeName: 'highway_assets:street_lights_reporting',
+            layerOptions: {
+                onEachFeature: () => { },
+                pointToLayer: (feature, latlng) => {
+                    return Leaflet.circleMarker(latlng, {
+                        color: '#000',
+                        weight: 2,
+                        opacity: 1,
+                        fillColor: '#33a02c',
+                        fillOpacity: 0.5
+                    })
+                }
             }
-        ]
+        }
+    ]
 }
--- Configuration/map-with-static-data/index.js
+++ Configuration/map-with-static-data/index.js
@@ -1,29 +1,8 @@
 export default {
     Map: {
-        StartingLatLng: [53.3915, -2.125143],
-        StartingZoom: 2,
-        EnableLocateControl: true,
-        Class: 'govuk-grid-column-full smbc-map__container'
+        StartingZoom: 2
     },
     Tiles: {
         Token: 'pk.eyJ1IjoiZ2lzLXN0b2NrcG9ydCIsImEiOiJja2xxcTUzZGUxZml1MnFxeTNidmt5c3kyIn0.QOsaZ7uPj4Kv2lwL63Crlg'
-    },
-    StaticData: 
-    [
-        {
-            key: 'boundary',
-            url: 'https://spatialgeojson.s3-eu-west-1.amazonaws.com/webmapping/boundary.geojson',
-            layerOptions: {
-                interactive: false,
-                maxZoom: 9,
-                style: {
-                    color: '#000',
-                    weight: 4,
-                    opacity: 1,
-                    fillColor: '#000',
-                    fillOpacity: 0
-                }
-            }
-        }
-    ]
+    }
 } 
