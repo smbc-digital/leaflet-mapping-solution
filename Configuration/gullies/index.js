7b963fea2c33cc9da1920f417643b8d82a433659
Lydia Gray <lydia.gray@stockport.gov.uk>
2021-05-07 08:50:27 +0100

    fix(staticData): idea for fixing static data being fetched regardless of zoom levels

--- Configuration/gullies/index.js
+++ Configuration/gullies/index.js
@@ -5,8 +5,7 @@ import { gulliesStyle} from './Styles'
 export default {
     Map: {
         Latitude: 53.391067,
-        Longitude: -2.1197936,
-        StartingZoom: 2,
+        Longitude: -2.1197936
     },
     Tiles: {
         Token: 'pk.eyJ1IjoiZ2lzLXN0b2NrcG9ydCIsImEiOiJja29jdWJ4MHIwMnczMnZsNHRtaWJkeHc2In0.W3-zhdnDhpyNX0AubRT--g'
@@ -21,24 +20,6 @@ export default {
                 onEachFeature: gulliesPopup,
                 pointToLayer: (feature, latlng) => {
                     return Leaflet.circleMarker (latlng, gulliesStyle (feature))
-                }
-            }
-        }
-    ],
-    StaticData: 
-    [
-        {
-            key: 'boundary',
-            url: 'https://spatialgeojson.s3-eu-west-1.amazonaws.com/webmapping/boundary.geojson',
-            layerOptions: {
-                interactive: false,
-                maxZoom: 9,
-                style: {
-                    color: '#000',
-                    weight: 4,
-                    opacity: 1,
-                    fillColor: '#000',
-                    fillOpacity: 0
                 }
             }
         }
