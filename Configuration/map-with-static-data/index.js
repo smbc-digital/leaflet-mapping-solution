12a1fade5811e459133161ac7e5d985ee5624867
stephen.lally <stephen.lally.smbc@gmail.com>
2021-05-07 09:54:28 +0100

    chore(config): disbale boundary, use static data

--- Configuration/map-with-static-data/index.js
+++ Configuration/map-with-static-data/index.js
@@ -1,6 +1,7 @@
 export default {
     Map: {
-        StartingZoom: 17
+        StartingZoom: 17,
+        DisplayBoundary: false
     },
     Tiles: {
         Token: 'pk.eyJ1IjoiZ2lzLXN0b2NrcG9ydCIsImEiOiJja29jdWJ4MHIwMnczMnZsNHRtaWJkeHc2In0.W3-zhdnDhpyNX0AubRT--g'
