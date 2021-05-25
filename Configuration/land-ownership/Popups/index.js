49e89ecebfa91ed15ab08638d53247b1d983703b
geospatial-dan <daniel.kinder@stockport.gov.uk>
2021-05-25 11:44:25 +0100

    chore: Changes to land-ownership popup

--- Configuration/land-ownership/Popups/index.js
+++ Configuration/land-ownership/Popups/index.js
@@ -1,26 +1,46 @@
 const Land_Ownership_Popup = (feature, layer) => {
-  const content = `<div class="item"><i class="fa fa-image"></i><p class="title">Land Ownership</p>
-    <p></p>
-    <p class="info">Committee: ${feature.properties.committee_new}</p>
-    <p class="info">Title Number: ${feature.properties.titlenumber}</p>
-    </div>`
-  layer.bindPopup(content)
+    const content = `<div class="smbc-map__item">
+  <div class="smbc-map__item__header__block">
+    <i class="fa fa-image smbc-map__item__header__block__icon" aria-hidden="true"></i>
+    <span class="smbc-map__item__header__block__title">Land Ownership</span>
+  </div>
+  <div class="smbc-map__item__body">
+    <p>Committee: ${feature.properties.committee_new}</p>
+    <p>Title Number: ${feature.properties.titlenumber}</p>
+  </div>
+  </div>`
+
+    layer.bindPopup(content)
 }
+
 const Leases_Popup = (feature, layer) => {
-  const content = `<div class="item"><i class="fa fa-file-text"></i><p class="title">Leases</p>
-    <p></p>
-    <p class="info">Description: ${feature.properties.description}</p>
-    </div>`
+  const content = `<div class="smbc-map__item">
+<div class="smbc-map__item__header__block">
+  <i class="fa fa-file-text smbc-map__item__header__block__icon" aria-hidden="true"></i>
+  <span class="smbc-map__item__header__block__title">Leases</span>
+</div>
+<div class="smbc-map__item__body">
+  <p>Committee: ${feature.properties.description}</p>
+</div>
+</div>`
+
   layer.bindPopup(content)
 }
-const Assets_Popup = (feature, layer) => {
-  const content = `<div class="item"><i class="fa fa-home"></i><p class="title">Council Owned Property Assets</p>
-    <p></p>
-    <p class="info">Name: ${feature.properties.epims_property_name}</p>
-    <p class="info">UPRN: ${feature.properties.epims_uprn}</p>
-    </div>`
-  layer.bindPopup(content)
-}
+
+const Assets_Popup = (feature, circleMarker) => {
+  circleMarker.bindPopup(`<div class="smbc-map__item">
+    <div class="smbc-map__item__header__block">
+      <i class="fa fa-home smbc-map__item__header__block__icon" aria-hidden="true"></i>
+      <span class="smbc-map__item__header__block__title">Council Owned Property Assets</span>
+    </div>
+    <div class="smbc-map__item__body">
+        <p>Name: ${feature.properties.epims_property_name}</p>
+        <p>UPRN: ${feature.properties.epims_uprn}</p>
+      </div>
+      </div>`
+    )
+  }
+
 export {
   Land_Ownership_Popup,
   Leases_Popup,
