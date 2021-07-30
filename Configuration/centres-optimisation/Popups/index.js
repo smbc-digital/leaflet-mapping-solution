25cb368b0bd71f4d8bd26b1bc83b2870f73d6e81
geospatial-dan <daniel.kinder@stockport.gov.uk>
2021-07-30 13:19:26 +0100

    Centres Optimisation map

---
+++ Configuration/centres-optimisation/Popups/index.js
@@ -0,0 +1,125 @@
+const gmalPopup = (feature, layer) => {
+  const content = `<div class="smbc-map__item">
+<div class="smbc-map__item__header__block">
+  <i class="fa fa-train smbc-map__item__header__block__icon" aria-hidden="true"></i>
+  <span class="smbc-map__item__header__block__title">GMAL Grid</span>
+</div>
+<div class="smbc-map__item__body">
+  <p>GMAL Level: ${feature.properties.gmallevel}</p>
+</div>
+</div>`
+
+  layer.bindPopup(content)
+}
+
+const landownershipPopup = (feature, layer) => {
+  const content = `<div class="smbc-map__item">
+<div class="smbc-map__item__header__block">
+  <i class="fa fa-image smbc-map__item__header__block__icon" aria-hidden="true"></i>
+  <span class="smbc-map__item__header__block__title">Land Ownership</span>
+</div>
+<div class="smbc-map__item__body">
+  <p>Name: ${feature.properties.property_name}</p>
+  <p>Committee: ${feature.properties.committee_new}</p>
+  <p>Title Number: ${feature.properties.titlenumber}</p>
+  <p>Area: ${feature.properties.area_sqm}</p>
+</div>
+</div>`
+
+  layer.bindPopup(content)
+}
+
+const cfsPopup = (feature, layer) => {
+  const content = `<div class="smbc-map__item">
+<div class="smbc-map__item__header__block">
+  <i class="fa fa-home smbc-map__item__header__block__icon" aria-hidden="true"></i>
+  <span class="smbc-map__item__header__block__title">CfS Submissions</span>
+</div>
+<div class="smbc-map__item__body">
+  <p>CfS Ref: ${feature.properties.cfsites_ref}</p>
+  <p>Year: ${feature.properties.year}</p>
+  <p>Data Source: ${feature.properties.data_source}</p>
+  <p>Site Address: ${feature.properties.site_address}</p>
+</div>
+</div>`
+
+  layer.bindPopup(content)
+}
+
+const boundariesPopup = (feature, layer) => {
+  const content = `<div class="smbc-map__item">
+<div class="smbc-map__item__header__block">
+  <i class="fa fa-building smbc-map__item__header__block__icon" aria-hidden="true"></i>
+  <span class="smbc-map__item__header__block__title">Centres Boundaries</span>
+</div>
+<div class="smbc-map__item__body">
+  <p>Type: ${feature.properties.type}</p>
+  <p>Name: ${feature.properties.policysitename}</p>
+</div>
+</div>`
+
+  layer.bindPopup(content)
+}
+
+const boundaries800Popup = (feature, layer) => {
+  const content = `<div class="smbc-map__item">
+<div class="smbc-map__item__header__block">
+  <i class="fa fa-building smbc-map__item__header__block__icon" aria-hidden="true"></i>
+  <span class="smbc-map__item__header__block__title">Centres Boundaries + 800m</span>
+</div>
+<div class="smbc-map__item__body">
+  <p>Within 800m of a District or Large Local Centre</p>
+</div>
+</div>`
+
+  layer.bindPopup(content)
+}
+
+const developablesitesPopup = (feature, layer) => {
+  
+  const content = `<div class="smbc-map__item">
+    <div class="smbc-map__item__header__block">
+      <i class="fas fa-home smbc-map__item__header__block__icon" aria-hidden="true"></i>
+      <span class="smbc-map__item__header__block__title">Sites - Developable (SHLAA 2020)</span>
+    </div>
+    <div class="smbc-map__item__body">
+      <p>Site Reference: ${feature.properties.site_reference}</p>
+      <p>Site Address: ${feature.properties.site_address}</p>
+      <p>Site Description: ${feature.properties.site_description_2020}</p>
+      <p>Site Area (Ha): ${feature.properties.site_area}</p>
+      <p>Est. Site Capacity: ${feature.properties.capacity}</p>
+      <p>Est. Delivery Years: ${feature.properties.delivery_years}</p>
+    </div>
+  </div>`
+ 
+  layer.bindPopup(content)
+ }
+
+ const nondevelopablesitesPopup = (feature, layer) => {
+  
+  const content = `<div class="smbc-map__item">
+    <div class="smbc-map__item__header__block">
+      <i class="fa fa-home smbc-map__item__header__block__icon" aria-hidden="true"></i>
+      <span class="smbc-map__item__header__block__title">Sites - Not Developable (SHLAA 2020)</span>
+    </div>
+    <div class="smbc-map__item__body">
+    <p>Site Reference: ${feature.properties.site_reference}</p>
+    <p>Site Address: ${feature.properties.site_address}</p>
+    <p>Site Description: ${feature.properties.site_description}</p>
+    <p>Site Area (Ha): ${feature.properties.site_area}</p>
+    <p>Notes: ${feature.properties.leaflet_summary}</p>
+    </div>
+  </div>`
+ 
+  layer.bindPopup(content)
+ }
+
+export {
+  gmalPopup,
+  landownershipPopup,
+  cfsPopup,
+  boundariesPopup,
+  boundaries800Popup,
+  developablesitesPopup,
+  nondevelopablesitesPopup
+}
---
+++ Configuration/centres-optimisation/Styles/index.js
@@ -0,0 +1,112 @@
+function getColour_gmal(d) {
+    switch  (d) {   case 1    :
+                        return '#d7191c'    
+                    case 2    :
+                        return '#ea633e'
+                    case 3    :
+                        return '#fdae61'
+                    case 4    :
+                        return '#fed791'
+                    case 5    :
+                        return '#ffffc0'
+                    case 6    :
+                        return '#d3ec95'
+                    case 7    :
+                        return '#a6d96a'
+                    case 8    :
+                        return '#60b855'
+    
+
+                }
+    }
+
+function gmalStyle (feature) {
+return {
+    color: '#000000',
+    weight: 2,
+    opacity: 0,
+    fillColor: getColour_gmal (feature.properties.gmallevel),
+    fillOpacity: 0.25
+    }
+}
+
+function getColor_AreaCommittees(d) {
+    switch 	(d) { 	case 'Corporate' : 
+                        return '#ffff00'
+                    case 'Education' : 
+                        return '#ffaf5f'
+                    case 'Greenspace' : 
+                        return '#55ff55'
+                    case 'Highways' : 
+                        return '#3255ff'
+                    case 'Social Services' :
+                        return '#c88cff'
+                    case 'Stockport Homes' : 
+                        return '#d70000'
+                    case 'Strategic Housing' : 
+                        return '#15ebf6'
+                    case 'Not Assigned' : 
+                        return '#000000'
+                    case 'Other' : 
+                        return '#000000'                   
+                }
+            }
+function landownershipStyle(feature) {
+    return {
+        fillColor: getColor_AreaCommittees(feature.properties.committee_new),
+        weight: 0,
+        opacity: 0,
+        color: 'black',
+        fillOpacity: 0.5
+        }
+    }
+
+const cfsStyle = {
+    color: '#e31a1c',
+    weight: 2,
+    opacity: 1,
+    fillColor: '#006400',
+    fillOpacity: 0
+}
+
+const boundariesStyle = {
+    color: '#1f78b4',
+    weight: 3,
+    opacity: 1,
+    fillColor: '#006400',
+    fillOpacity: 0
+}
+
+const boundaries800Style = {
+    color: '#6a3d9a',
+    weight: 3,
+    opacity: 1,
+    fillColor: '#6a3d9a',
+    fillOpacity: 0.25
+}
+
+const nondevelopablesitesStyle = {
+    color: '#ff454e',
+    weight: 3,
+    opacity: 1,
+    fillColor: '#ff454e',
+    fillOpacity: 0.5
+}
+
+const developablesitesStyle = {
+    color: '#04c427',
+    weight: 3,
+    opacity: 1,
+    fillColor: '#04c427',
+    fillOpacity: 0.5
+}
+
+export {
+    gmalStyle,
+    landownershipStyle,
+    cfsStyle,
+    boundariesStyle,
+    boundaries800Style,
+    developablesitesStyle,
+    nondevelopablesitesStyle
+}
