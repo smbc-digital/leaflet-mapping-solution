57b624e0cf9fc0db5c968104bf967507766470b3
SCN\christopher.lang <christopher.lang@stockport.gov.uk>
2021-08-06 12:07:40 +0100

    prow 1st push

---
+++ Configuration/prow/index.js
@@ -0,0 +1,31 @@
+import Leaflet from 'leaflet'
+import {prowPopup} from './Popups'
+import {prowStyle} from './Styles'
+
+const Configuration = {
+    Map: {
+
+    },
+
+    Tiles: {
+        Token: '3G26OzBg7XRROryDwG1o1CZRmIx66ulo'
+    }, 
+
+    DynamicData: 
+    [
+
+        {
+            key: 'Public Rights of Way',
+            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=highways:public_rights_of_way&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
+            layerOptions: {
+                onEachFeature: prowPopup,
+                style: prowStyle,
+
+            },
+            displayInOverlay: false
+        },
+        
+    ],
+}
+
+export default Configuration
