45aa01176f4949d1eead56b18cf94e8ff1edb925
geospatial-dan <daniel.kinder@stockport.gov.uk>
2021-08-09 15:00:01 +0100

    Brownfield Map - Popup Fix

--- Configuration/brownfields-map/Popups/index.js
+++ Configuration/brownfields-map/Popups/index.js
@@ -2,27 +2,14 @@
 const brownfield2019Popup = (feature, layer) => {
   
   const content = `<div class="smbc-map__item">
-    <p class="smbc-body smbc-!-font-color-white">Brownfield Land Register 2019</p>
-    <div class="smbc-map__item__header__block">
-      <i class="fa fa-tag smbc-map__item__header__block__icon" aria-hidden="true"></i>
-      <span class="smbc-map__item__header__block__title">Reference: </span>
-    </div>
+  <div class="smbc-map__item__header__block">  
+    <i class="fa fa-tag smbc-map__item__header__block__icon" aria-hidden="true"></i>
+  <span class="smbc-map__item__header__block__title">Brownfield Land Register 2019</span>
+  </div>
     <div class="smbc-map__item__body">
-      <p>${feature.properties.sitereference}</p>
-    </div>
-    <div class="smbc-map__item__header__block">
-      <i class="fa fa-map-marker smbc-map__item__header__block__icon" aria-hidden="true"></i>
-      <span class="smbc-map__item__header__block__title">Address:</span>
-    </div>
-    <div class="smbc-map__item__body">
-      <p>${feature.properties.sitenameaddress.replace(/"/g, '')}</p>
-    </div>
-    <div class="smbc-map__item__header__block">
-      <i class="fa fa-home smbc-map__item__header__block__icon" aria-hidden="true"></i>
-      <span class="smbc-map__item__header__block__title">Net Dwellings:</span>
-    </div>
-    <div class="smbc-map__item__body">
-      <p>${feature.properties.netdwellingsrangeto}</p>
+      <p>Reference: ${feature.properties.sitereference}</p>
+      <p>Address: ${feature.properties.sitenameaddress.replace(/"/g, '')}</p>
+      <p>Net Dwellings: ${feature.properties.netdwellingsrangeto}</p>
     </div>
   </div>`
 
@@ -32,33 +19,19 @@ const brownfield2019Popup = (feature, layer) => {
 const brownfield2020Popup = (feature, layer) => {
   
   const content = `<div class="smbc-map__item">
-    <p class="smbc-body smbc-!-font-color-white">Brownfield Land Register 2020</p>
-    <div class="smbc-map__item__header__block">
-      <i class="fa fa-tag smbc-map__item__header__block__icon" aria-hidden="true"></i>
-      <span class="smbc-map__item__header__block__title">Reference: </span>
-    </div>
+  <div class="smbc-map__item__header__block">  
+    <i class="fa fa-tag smbc-map__item__header__block__icon" aria-hidden="true"></i>
+  <span class="smbc-map__item__header__block__title">Brownfield Land Register 2020</span>
+  </div>
     <div class="smbc-map__item__body">
-      <p>${feature.properties.sitereference}</p>
-    </div>
-    <div class="smbc-map__item__header__block">
-      <i class="fa fa-map-marker smbc-map__item__header__block__icon" aria-hidden="true"></i>
-      <span class="smbc-map__item__header__block__title">Address:</span>
-    </div>
-    <div class="smbc-map__item__body">
-      <p>${feature.properties.sitenameaddress.replace(/"/g, '')}</p>
-    </div>
-    <div class="smbc-map__item__header__block">
-      <i class="fa fa-home smbc-map__item__header__block__icon" aria-hidden="true"></i>
-      <span class="smbc-map__item__header__block__title">Net Dwellings:</span>
-    </div>
-    <div class="smbc-map__item__body">
-      <p>${feature.properties.netdwellingsrangefrom}</p>
+      <p>Reference: ${feature.properties.sitereference}</p>
+      <p>Address: ${feature.properties.sitenameaddress.replace(/"/g, '')}</p>
+      <p>Net Dwellings: ${feature.properties.netdwellingsrangefrom}</p>
     </div>
   </div>`
 
   layer.bindPopup(content)
 }
-
 
 export {
     brownfield2019Popup,
