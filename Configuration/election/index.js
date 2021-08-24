8785d8920776c1740ebc50cb7412b4d2a7bf35b3
geospatial-dan <daniel.kinder@stockport.gov.uk>
2021-08-24 09:56:35 +0100

    Election map - formatting edits

--- Configuration/election/index.js
+++ Configuration/election/index.js
@@ -4,7 +4,7 @@ import { wardPopup, polling_districtsPopup, polling_stationsPopup } from './Popu
 
 export default {
     Map: {
-        MapClickMinZoom: 16
+
     },
     Tiles: {
         Token: '3G26OzBg7XRROryDwG1o1CZRmIx66ulo'
@@ -16,36 +16,27 @@ export default {
                 url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=political:polling_districts&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
                 layerOptions: {
                     onEachFeature: polling_districtsPopup,
-                    maxZoom: 2,
                     style: polling_districtsStyle
                 },
-                displayOverlay: true,
-                visibleByDefault: true
             },
             {
                 key: 'Ward',
                 url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=political:ward&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
                 layerOptions: {
                     onEachFeature: wardPopup,
-                    maxZoom: 2,
                     style: wardStyle
                 },
-                displayOverlay: true,
-                visibleByDefault: true
             },
             {
                 key: 'Polling Stations',
                 url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=political:polling_stations_4326&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
                 layerOptions: {
                     onEachFeature: polling_stationsPopup,
-                    maxZoom: 2,
                     style: polling_stationStyle,
                     pointToLayer: (feature, latlng) => {
                         return Leaflet.circleMarker(latlng)
                     }
                 },
-                displayOverlay: true,
-                visibleByDefault: true
             },
         ]
 }
