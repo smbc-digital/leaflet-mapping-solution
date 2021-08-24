3cf7b5e507ac72566f7f562fb0d0a6c002d86e15
Colin Lees <colinleesa7x@gmail.com>
2021-08-24 11:09:02 +0100

    feat: add GestureControl for UX testing

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 const { DefinePlugin } = require('webpack')
 
-const solution = 'election'
+const solution = 'map-with-dynamic-data'
 
 module.exports = (env, argv, t) => (
     {
