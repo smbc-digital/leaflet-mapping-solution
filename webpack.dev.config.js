dccad1ee620c2025052c77cc54ab10ad678c91ad
SCN\christopher.lang <christopher.lang@stockport.gov.uk>
2021-09-06 16:40:39 +0100

    first push of leisure centres map

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 const { DefinePlugin } = require('webpack')
 
-const solution = 'early-years'
+const solution = 'leisure-centres'
 
 module.exports = (env, argv, t) => (
     {
