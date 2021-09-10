57d1a81cfb357de5566fee53e84ac08ec45ccaf2
SCN\christopher.lang <christopher.lang@stockport.gov.uk>
2021-09-10 16:04:39 +0100

    first push of smoke control map

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 const { DefinePlugin } = require('webpack')
 
-const solution = 'gritting'
+const solution = 'smoke-control'
 
 module.exports = (env, argv, t) => (
     {
