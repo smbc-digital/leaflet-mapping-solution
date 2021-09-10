73a16bb3f3f808c85ac9570bfe73bdce3b353bba
SCN\christopher.lang <christopher.lang@stockport.gov.uk>
2021-09-10 16:23:54 +0100

    changed smoke control popup icon

--- Configuration/smoke-control/Popups/index.js
+++ Configuration/smoke-control/Popups/index.js
@@ -2,7 +2,7 @@ const smokecontrolareaPopup = (feature, layer) => {
   
   const content = `<div class="smbc-map__item">
     <div class="smbc-map__item__header__block">
-      <i class="fas fa-tag smbc-map__item__header__block__icon" aria-hidden="true"></i>
+      <i class="fas fa-smog smbc-map__item__header__block__icon" aria-hidden="true"></i>
       <span class="smbc-map__item__header__block__title">Smoke Control Area</span>
     </div>
     <div class="smbc-map__item__body">
