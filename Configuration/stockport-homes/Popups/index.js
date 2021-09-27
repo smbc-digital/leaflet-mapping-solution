2c907786f798703f7bbac70c7e1965d67688727d
Jaime Scowcroft <jaime.scowcroft@stockport.gov.uk>
2021-09-27 21:13:13 +0100

    Stockport Homes first push

---
+++ Configuration/stockport-homes/Popups/index.js
@@ -0,0 +1,47 @@
+const FEC_Popup = (feature, layer) => {
+  const content = `<div class="smbc-map__item">
+<div class="smbc-map__item__header__block">
+  <i class="fa fa-users smbc-map__item__header__block__icon" aria-hidden="true"></i>
+  <span class="smbc-map__item__header__block__title">Family Education Cluster</span>
+</div>
+<div class="smbc-map__item__body">
+  <p>${feature.properties.family_education_cluster}</p>
+</div>
+</div>`
+
+  layer.bindPopup(content)
+}
+
+const eyProviderPopup = (feature, layer) => {
+  const content = `<div class="smbc-map__item">
+<div class="smbc-map__item__header__block">
+  <i class="fa fa-child smbc-map__item__header__block__icon" aria-hidden="true"></i>
+  <span class="smbc-map__item__header__block__title">EY Provider</span>
+</div>
+<div class="smbc-map__item__body">
+  <p>${feature.properties.provider_name}</p>
+  
+</div>
+</div>`
+layer.bindPopup(content)
+}
+
+const childminderPopup = (feature, layer) => {
+  const content = `<div class="smbc-map__item">
+<div class="smbc-map__item__header__block">
+  <i class="fa fa-home smbc-map__item__header__block__icon" aria-hidden="true"></i>
+  <span class="smbc-map__item__header__block__title">Childminder</span>
+</div>
+<div class="smbc-map__item__body">
+  <p>${feature.properties.provider_name}</p>
+  
+</div>
+</div>`
+layer.bindPopup(content)
+}
+
+export {
+  FEC_Popup,
+  eyProviderPopup,
+  childminderPopup
+}
---
+++ Configuration/stockport-homes/index.js
@@ -0,0 +1,82 @@
+import Leaflet from 'leaflet'
+import {eyProviderPopup, FEC_Popup, childminderPopup} from './Popups'
+import {Leasesstyle} from './Styles'
+
+const Configuration = {
+    Map: {
+        StartingZoom: 13
+    },
+    Tiles: {
+        Token: '3G26OzBg7XRROryDwG1o1CZRmIx66ulo'
+    },
+    DynamicData: 
+    [
+
+        {
+            key: 'fec_wms',
+            url: 'http://spatial.stockport.gov.uk/geoserver/wms?',
+            layerOptions: {
+               maxZoom: 20,
+               minZoom: 11,
+               layers: 'political:family_edu_cluster',
+               format: 'image/png',
+               transparent: true
+            },
+            displayInOverlay: false
+             },
+    
+        {
+            key: 'Family Education Clusters',
+            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=political:family_edu_cluster&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
+            layerOptions: {
+                onEachFeature: FEC_Popup,
+                style: Leasesstyle
+            },
+            visibleByDefault: true
+        },
+
+        {
+            key: 'Early Years Provider',
+            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=education:ey_provider&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
+            layerOptions: {
+                onEachFeature: eyProviderPopup,
+                pointToLayer: (feature, latlng) => {
+                    return Leaflet.circleMarker(latlng, {
+                        color: '#000',
+                        weight: 2,
+                        opacity: 1,
+                        fillColor: '#33a02c',
+                        fillOpacity: 0.5
+                    })
+                }
+                
+            },
+            visibleByDefault: true
+        },
+        
+ //       {
+ //           key: 'Early Years Childminder',
+ //           url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=education:ey_childminder&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
+ //           layerOptions: {
+ //               onEachFeature: childminderPopup,
+ //               pointToLayer: (feature, latlng) => {
+ //                   return Leaflet.circleMarker(latlng, {
+//                        color: '#000',
+//                        weight: 2,
+//                        opacity: 1,
+//                        fillColor: '#33a02c',
+//                        fillOpacity: 0.5
+//                    })
+//                }
+//                
+//            },
+//            visibleByDefault: false
+//     
+//        },
+
+       
+        
+    ]
+}
+
+export default Configuration
