1f7ebe74c0d124d8fceb12732f685807b50506c4
Jaime Scowcroft <jaime.scowcroft@stockport.gov.uk>
2021-10-05 13:37:16 +0100

    mcf map - first push

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 const { DefinePlugin } = require('webpack')
 
-const solution = 'illuminated-assets'
+const solution = 'mcf'
 
 module.exports = (env, argv, t) => (
     {
