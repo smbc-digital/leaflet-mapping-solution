08c4b25292fea6276e2532cf2b30260e11857466
SCN\christopher.lang <christopher.lang@stockport.gov.uk>
2021-10-15 08:39:25 +0100

    Added layers to teh two-finger maps

--- Configuration/test-map-two-fingers/Popups/index.js
+++ Configuration/test-map-two-fingers/Popups/index.js
@@ -16,6 +16,37 @@ const leisurecentresPopup = (feature, layer) => {
   layer.bindPopup(content)
 }
 
+const prowPopup = (feature, layer) => {
+  const content = `<div class="smbc-map__item">
+<div class="smbc-map__item__header__block">
+  <i class="fa fa-map-signs smbc-map__item__header__block__icon" aria-hidden="true"></i>
+  <span class="smbc-map__item__header__block__title">Public Right of Way</span>
+</div>
+<div class="smbc-map__item__body">
+<p>PROW Number: ${feature.properties.row}</p>
+<p>Type: ${feature.properties.type}</p>
+</div>
+</div>`
+    
+    layer.bindPopup(content)
+}
+
+const Conservation_area_Popup = (feature, layer) => {
+  const content = `<div class="smbc-map__item">
+  <div class="smbc-map__item__header__block">
+    <i class="fa fa-tag smbc-map__item__header__block__icon" aria-hidden="true"></i>
+    <span class="smbc-map__item__header__block__title">Conservation Area</span>
+  </div>
+  <div class="smbc-map__item__body">
+    <p>Name: ${feature.properties.cons_area}</p>
+    <a href="${feature.properties.web_info_link}" target="_blank">Further Information</a>
+    </div>
+    </div>`
+
+  layer.bindPopup(content)
+}
+
+
 export {
-  leisurecentresPopup
+  leisurecentresPopup, prowPopup, Conservation_area_Popup
 }
--- Configuration/test-map-two-fingers/index.js
+++ Configuration/test-map-two-fingers/index.js
@@ -1,6 +1,6 @@
 import Leaflet from 'leaflet'
-import { leisurecentresPopup } from './Popups'
-import { leisurecentresStyle } from './Styles'
+import { leisurecentresPopup, prowPopup, Conservation_area_Popup } from './Popups'
+import { leisurecentresStyle, prowStyle, Conservation_area_style } from './Styles'
 
 const Configuration = {
     Map: {
@@ -21,6 +21,26 @@ const Configuration = {
                     pointToLayer: (feature, latlng) => {
                         return Leaflet.circleMarker(latlng)
                     }
+                }
+            },
+
+            {
+                key: 'Public Rights of Way',
+                url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=highways:public_rights_of_way&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
+                layerOptions: {
+                    onEachFeature: prowPopup,
+                    style: prowStyle,
+    
+                }    
+            },
+
+            {
+                key: 'Conservation Area',
+                url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=heritage:conservation_area&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
+                layerOptions: {
+                    onEachFeature: Conservation_area_Popup,
+                    maxZoom: 2,
+                    style: Conservation_area_style
                 }
             }
         ]
