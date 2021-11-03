93e3d63486fd453fad77e8ccb98059972e0c9019
SCN\christopher.lang <christopher.lang@stockport.gov.uk>
2021-11-03 11:30:49 +0000

    Updated emergency planning map

--- Configuration/emergency_planning/index.js
+++ Configuration/emergency_planning/index.js
@@ -1,6 +1,6 @@
 import Leaflet from 'leaflet'
-import {careHomePopup} from './Popups'
-import {careHomeStyle} from './Styles'
+import {careHomePopup,cat1CareHomePopup,cat2CareHomePopup,exrpCareHomePopup} from './Popups'
+import {careHomeStyle,cat1CareHomeStyle,cat2CareHomeStyle,exrpCareHomeStyle} from './Styles'
 
 const Configuration = {
     Map: {
@@ -20,6 +20,48 @@ const Configuration = {
             layerOptions: {
                 onEachFeature: careHomePopup,
                 style: careHomeStyle,
+                pointToLayer: (feature, latlng) => {
+                    return Leaflet.circleMarker(latlng)
+                },
+
+            },
+            
+        },
+
+        {
+            key: 'Cat 1 Care Homes',
+            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=buildings_and_land:category_1_care_homes&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
+            layerOptions: {
+                onEachFeature: cat1CareHomePopup,
+                style: cat1CareHomeStyle,
+                pointToLayer: (feature, latlng) => {
+                    return Leaflet.circleMarker(latlng)
+                },
+
+            },
+            
+        },
+
+        {
+            key: 'Cat 2 Care Homes',
+            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=buildings_and_land:category_2_care_homes&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
+            layerOptions: {
+                onEachFeature: cat2CareHomePopup,
+                style: cat2CareHomeStyle,
+                pointToLayer: (feature, latlng) => {
+                    return Leaflet.circleMarker(latlng)
+                },
+
+            },
+            
+        },
+
+        {
+            key: 'Extra care and private Care Homes',
+            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=buildings_and_land:extra_care_rp_and_private_care_homes&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
+            layerOptions: {
+                onEachFeature: exrpCareHomePopup,
+                style: exrpCareHomeStyle,
                 pointToLayer: (feature, latlng) => {
                     return Leaflet.circleMarker(latlng)
                 },
