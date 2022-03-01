f627bb87cc12b28eb7600f329281a9f18bb7e55c
Jon Chiles <jonchiles@yahoo.com>
2022-03-01 15:06:43 +0000

    chore(story): added story points for town centre

--- Configuration/proptech-iteration-one/index.js
+++ Configuration/proptech-iteration-one/index.js
@@ -13,24 +13,84 @@ export default {
         Token: '3G26OzBg7XRROryDwG1o1CZRmIx66ulo' //needs a new token?
     },
     Stages:
+
     [
+
         {
+
             key: 0,
-            layers:['Town Centre Boundary', 'Main Town Centre Use Area', 'Town Centre Character Areas','Borough - 1km'],
+            layers:['Town Centre Boundary', 'Borough - 1km'],
             zoom: 12,
-            latitude: 53.39205825732247,
-            longitude: -2.1383008755576998,
-            narrativeTitle: "one",
-            narrative: '<p>Wells Cathedral is an Anglican cathedral in Wells, Somerset, commenced around 1175 on the site of a late-Roman mausoleum and an 8th-century abbey church. The cathedral has been described by the historian John Harvey as Europe"s first truly Gothic structure, lacking the Romanesque work that survives in many other cathedrals. It is the seat of the bishop of Bath and Wells. This photograph depicts the interior of Wells Cathedral"s chapter house, built by unknown architects between 1275 and 1310 in the Geometric style of Decorated Gothic architecture.</p>',
+            latitude: 53.40692246375874,
+            longitude: -2.162584935245935,
+            narrativeTitle: 'Town Centre 1: Stockport Town Centre Principles',
+            narrative: '<p><b>Evolving and expanding the role of the Town Centre</b><br/>The Council, working with its partners, will work to grow the importance and appeal of Stockport Town Centre both economically, and as a local hub for all ages. With sustainability and accessibility as a guideline we will create a thriving, high quality, and higher density town centre in which to live, work and play.<br/>Developments within the town centre will be encourage a rich diversity, and liveliness to the area. New developments will:'
+                + '<ul>'
+				+ '<li>Ensure 6,000 new homes between 2021 and 2038</li>'
+				+ '<li>Increase the number and variety of job opportunities available</li>'
+				+ '<li>Provide modern and and attractive offices suitable for both traditional and flexible working habits</li>'
+				+ '<li>Expand and improve upon the availability of important neighbourhood facilities, including social and community infrastructure</li>'
+				+ '<li>Offer a wider range of cultural, social, and tourism activities drawing people to the Town Centre throughout the day and into the evening</li>'
+				+ '<li>Complement and make best use of the town centres existing transport links, and improve connectivity within the town centre with an emphasis on walking, cycling and public transport.</li>'
+				+ '<li>Take account of the council’s commitment to net zero carbon emissions by 2038.</li>'
+                + '</ul></p>',
+
         },
         {
             key: 1,
-            layers:['Town Centre Boundary', 'Main Town Centre Use Area', 'Town Centre Character Areas', 'Borough - 1km'],            
+            layers:['Town Centre Boundary', 'Borough - 1km'],            
             zoom: 16,
-            latitude: 53.384784,
-            longitude: -2.092305,
-            narrativeTitle: "two",
-            narrative: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mollis elementum tortor sit amet elementum. Mauris tristique, neque non maximus eleifend, nisl orci tincidunt metus, suscipit vehicula nibh est id orci. Vestibulum convallis ac turpis at varius. Maecenas placerat pellentesque erat quis ultrices. Aenean ultrices iaculis lectus vitae egestas. Sed purus arcu, tincidunt sit amet facilisis sed, posuere at urna. Nulla non ipsum eget ex scelerisque feugiat. Aliquam sit amet venenatis turpis. Proin eget dolor id nisi imperdiet imperdiet.</p><p>Ut suscipit finibus mauris, ac lacinia odio placerat vitae. Donec porttitor augue nibh, semper fermentum odio imperdiet in. Quisque viverra nulla quis tortor consequat, non ullamcorper lorem scelerisque. Quisque semper leo id magna aliquam, eget ultrices purus scelerisque. Maecenas fermentum condimentum ligula, blandit luctus metus volutpat ut. Quisque scelerisque tellus nec lectus pharetra commodo. Quisque porta, purus vel elementum gravida, lorem purus condimentum leo, venenatis sodales sem lacus at erat. Maecenas vehicula mauris sed ex gravida rhoncus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Etiam tristique vitae magna non fringilla. Sed justo nunc, viverra in vehicula eget, venenatis non turpis. Suspendisse molestie odio ut dolor convallis ultrices. Vestibulum ac lobortis lorem. Donec nec tellus velit.</p>',
+            latitude: 53.40685850612831,
+            longitude: -2.1627790770209914,
+            narrativeTitle: 'Town Centre 1: Stockport Town Centre Principles',
+            narrative: '<p><b>Key Areas of Change</b><br/>In order to help achieve our aims redevelopment and regeneration projects, some already underway, will take place in key town centre locations at:'
+                + '<ul>'
+				+ '<li>Mersey Way, where work is underway to bring about new uses within existing units and further development will bring new leisure, retail, employment, library and community facilities to the area.</li>'
+				+ '<li>The Interchange, where a state-of-the-art transport hub will be complemented with high-quality homes, a new park, and pedestrian links to Merseyway and Stockport Station.</li>'
+				+ '<li>Weir Mill, where historic industrial buildings will provide the basis for a new community and cultural destination on the banks of the River Mersey.</li>'
+				+ '<li>The Stockport Exchange, will become the town’s premier business location. Boasting exceptional transport links, high-specification office space, a hotel, and commercial businesses and car parking infrastructure.</li>'
+				+ '<li>Stockport Station, around which work will be undertaken to alleviate connectivity issues caused by the railway and its sidings.</li>'
+				+ '<li>Royal George Village, where amendments to the Stockport College estate and a repurposing of the Grade II listed former Metropolitan Girls’ School will enable development of a new, inclusive, neighbourhood of homes, offices, and commercial spaces on the edge of the town centre.</li>'
+                + '</ul></p>',
+        },
+	{
+            key: 2,
+            layers:['Town Centre Boundary', 'Borough - 1km'],            
+            zoom: 16,
+            latitude: 53.409148129400336,
+            longitude: -2.1531660352850173,
+            narrativeTitle: 'Town Centre 1: Stockport Town Centre Principles',
+
+            narrative: '<p><b>A hub for culture and lifestyle</b><br/>The Council will promote and enable the town centre to grow as cultural hub, supporting activities that encourage social interaction and provide opportunities for extended leisure time. This will be achieved through:' 
+			+ '<ul>'
+            + '<li>the modernisation and upkeep of venues, museums, and other facilities important to Stockport’s visitor economy.</li>'
+            + '<li>the diversification and improvement of the available leisure and commercial gym offering in the town centre. </li>'
+            + '<li>new and improved markets, outdoor events and fairs.</li>'
+            + '<li>grow and improve the variety of Stockport’s night life, increasing the number and range of restaurants and amenities available, and how the town centres outdoor spaces are used in the evenings.</li>'
+            + '<li>bringing Stockport’s history and heritage sites to the forefront.</li>'
+            + '<li>allowing for development to meet the demand for hotels and conference facilities.</li>'
+            + '<li>enabling the temporary use of vacant buildings and sites for creative, cultural and community organisations, especially where they can help to bring new life to these spaces. </li><br/>'
+			+ '</ul></p>'
+			+ '<p><b>Optimising the Town Centre’s development potential</b><br/>'
+			+ 'The Council will support opportunities to develop high-quality homes, creating and regenerating areas in a highly accessible and sustainable location. Any residential development in the town centre will be held to rigorous space and amenity standards, and must make efficient use of the available land. There are opportunities for taller buildings, at key strategic areas around the town centre.'
+			+ '</p>',
+        },
+	{
+            key: 3,
+            layers:['Town Centre Boundary','Main Town Centre Use Area', 'Borough - 1km'],            
+            zoom: 16,
+            latitude: 53.406410800022975,
+            longitude: -2.1586095333398903,
+            narrativeTitle: 'Town Centre 1: Stockport Town Centre Principles',
+            narrative: '<p><b>Improving the Town Centre public realm and environment</b><br/>The town centre will have public spaces and facilities that help to foster a welcoming and safe place for people visiting and living within the town centre. Development proposals within the town centre will:'
+			+ '<ul>'
+            + '<li>create inviting and safe places for pedestrians, cyclists, and the mobility impaired</li>'
+            + '<li>create high-quality streets, public spaces, and attractive routes helping people to move around the town centre</li>'
+            + '<li>will introduce environmental and biodiversity improvements to the character and appearance of the town</li>'
+			+ '</ul></p>'
+			+ '<p><b>Main town centre uses and retail development</b><br/>'
+			+ 'Retail, leisure and entertainment, offices, and tourism development will be directed towards the ‘Main Town Centre Use Area’. Retail development, specifically, will be directed towards the Primary Shopping Area within the town centre, as defined elsewhere in the Local Plan.'
+			+ '</p>',
         }
     ],
     DynamicData: 
