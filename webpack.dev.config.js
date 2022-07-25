2ab2dd6ec46e22c983130dddef2cf0cf19d65efb
Jon Chiles <jonchiles@yahoo.com>
2022-07-25 11:39:48 +0100

    chore(our inf): getting narrative from scnwebappsdev1

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 const { DefinePlugin } = require('webpack')
 
-const solution = 'story-maps-our-environment'
+const solution = 'story-maps-our-infrastructure'
 
 module.exports = (env, argv, t) => (
     {
