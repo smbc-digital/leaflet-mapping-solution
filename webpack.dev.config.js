8a8aa8893fc7a0a11684c329214798c62c5dc186
Jon Chiles <jonchiles@yahoo.com>
2022-07-29 13:57:33 +0100

    chore(approaches): reworking index.js to see if they work

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 const { DefinePlugin } = require('webpack')
 
-const solution = 'story-maps-our-homes'
+const solution = 'story-maps-housing-approach'
 
 module.exports = (env, argv, t) => (
     {
