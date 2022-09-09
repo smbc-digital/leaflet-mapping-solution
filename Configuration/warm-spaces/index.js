ba38e53016fb18383e5896506d2946f227a98eb8
Jaime Scowcroft <jaime.scowcroft@stockport.gov.uk>
2022-09-09 11:18:46 +0100

    warm spaces

---
+++ Configuration/warm-spaces/index.js
@@ -0,0 +1,65 @@
+import Leaflet from "leaflet";
+import {communityPopup,librariesPopup} from "./Popups";
+import {communityStyle,librariesStyle} from "./Styles";
+
+export default {
+  Map: {
+    Latitude: 53.39205825732247,
+    Longitude: -2.1383008755576998,
+    StartingZoom: 12
+  },
+  Tiles: { Token: "3G26OzBg7XRROryDwG1o1CZRmIx66ulo" },
+  LayerControlOptions: { keyGraphic: true },
+  DynamicData: [
+    {
+      key: 'Libraries',
+      url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=buildings_and_land:library&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
+      layerOptions: {
+        onEachFeature: librariesPopup,
+          style: librariesStyle,
+          pointToLayer: (feature, latlng) => {
+              return Leaflet.circleMarker(latlng)
+          },
+
+      }
+  },
+    {
+      key: "Community Buildings",
+      url: "https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=community:community_buildings_warm_place&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326",
+      visibleByDefault: true,
+      layerOptions: {
+        minZoom: 20,
+        maxZoom: 2,
+        onEachFeature: communityPopup,
+        style: communityStyle,
+        pointToLayer: (_, latlng) => {
+          return Leaflet.circleMarker(latlng)
+        },
+        key: {
+          type: 'array',
+          graphic: [
+            { text: 'Stockport Homes', style: 'fill:#ff0000;stroke:black;stroke-width:1;' },
+            { text: 'SMBC', style: 'fill:#00ff00;stroke:black;stroke-width:1;' },
+            { text: 'Committee', style: 'fill:#0000ff;stroke:black;stroke-width:1;' }
+          ]
+        }
+      }
+    },
+    
+    {
+      key: 'Wards',
+      url: 'wms',
+      visibleByDefault: false,
+      layerOptions: {
+          layers: 'political:ward',
+          popup: { 
+              icon: 'fa fa-square-o',
+              body: {
+                  'Name': 'ward_name',
+              }
+          }
+      },
+  }
+    
+  ]
+}
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 const { DefinePlugin } = require('webpack')
 
-const solution = 'polling-district-review-internal'
+const solution = 'warm-spaces2'
 
 module.exports = (env, argv, t) => (
     {
