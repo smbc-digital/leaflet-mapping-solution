0116640f3e974ffeb484f5978bdb353ed9cb4a34
TomWood116 <thomas.wood@stockport.gov.uk>
2022-09-14 15:02:19 +0100

    More layers for SA web map

--- Configuration/planning-sustainability-appraisal/index.js
+++ Configuration/planning-sustainability-appraisal/index.js
@@ -497,6 +497,565 @@ const Configuration = {
                     }
                 }
             },
+        },
+
+        {
+            key: 'Percentage Travelled Less Than 5 km',
+            group: 'Economic - Distance Workplace Population Travelled',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_dwwp_less_than_5km',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Percentage': 'perc_less_5km'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Percentage Travelled Between 5 and 10 km',
+            group: 'Economic - Distance Workplace Population Travelled',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_dwwp_5to10km',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Percentage': 'perc_5km_to_10km'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Percentage Travelled Between 10 and 30 km',
+            group: 'Economic - Distance Workplace Population Travelled',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_dwwp_10to30km',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Percentage': 'perc_10km_to_30km'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Percentage Travelled Between 30 and 60 km',
+            group: 'Economic - Distance Workplace Population Travelled',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_dwwp_30to60km',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Percentage': 'perc_30km_to_60km'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Percentage Travelled More Than 60 km',
+            group: 'Economic - Distance Workplace Population Travelled',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_dwwp_60km_plus',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Percentage': 'perc_60km_plus'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Percentage Work at Home',
+            group: 'Economic - Distance Workplace Population Travelled',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_dwwp_work_at_home',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Percentage': 'perc_work_at_home'
+                    }
+                }
+            },
+        },
+
+        {
+            key: 'Estimated Total Population (Mid 2019)',
+            group: 'Social - Population',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_pe_estimated_pop',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Total Population': 'all_ages'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Population Density',
+            group: 'Social - Population',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:population_density_lsoa',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'People per Square km': 'people_per_sq_km'
+                    }
+                }
+            },
+        },
+
+        {
+            key: 'Average Age (All Persons)',
+            group: 'Social - Population - Estimated Age Demographics',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_ad_all_persons_av_age',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Average Age': 'average_age'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Percentage Aged 0-18 (All Persons)',
+            group: 'Social - Population - Estimated Age Demographics',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_ad_all_persons_0to18',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Percentage': 'perc_0_18'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Percentage Aged 19-64 (All Persons)',
+            group: 'Social - Population - Estimated Age Demographics',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_ad_all_persons_19to64',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Percentage': 'perc_19_64'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Percentage Aged 65+ (All Persons)',
+            group: 'Social - Population - Estimated Age Demographics',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_ad_all_persons_65plus',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Percentage': 'perc_65_plus'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Average Age (Male)',
+            group: 'Social - Population - Estimated Age Demographics',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_ad_male_av_age',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Average Age': 'male_average_age'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Percentage Aged 0-18 (Male)',
+            group: 'Social - Population - Estimated Age Demographics',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_ad_male_0to18',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Percentage': 'male_perc_0_18'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Percentage Aged 19-64 (Male)',
+            group: 'Social - Population - Estimated Age Demographics',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_ad_male_19to64',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Percentage': 'male_perc_19_64'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Percentage Aged 65+ (Male)',
+            group: 'Social - Population - Estimated Age Demographics',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_ad_male_65plus',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Percentage': 'male_perc_65_plus'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Average Age (Female)',
+            group: 'Social - Population - Estimated Age Demographics',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_ad_female_av_age',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Average Age': 'female_average_age'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Percentage Aged 0-18 (Female)',
+            group: 'Social - Population - Estimated Age Demographics',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_ad_female_0to18',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Percentage': 'female_perc_0_18'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Percentage Aged 19-64 (Female)',
+            group: 'Social - Population - Estimated Age Demographics',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_ad_female_19to64',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Percentage': 'female_perc_19_64'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Percentage Aged 65+ (Female)',
+            group: 'Social - Population - Estimated Age Demographics',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_ad_female_65plus',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Percentage': 'female_perc_65_plus'
+                    }
+                }
+            },
+        },
+
+        {
+            key: 'Percentage White British',
+            group: 'Social - Population - Ethnicity',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_eth_white_british',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Percentage': 'perc_white_english_scottish_nirish_british'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Percentage White Irish',
+            group: 'Social - Population - Ethnicity',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_eth_white_irish',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Percentage': 'perc_white_irish'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Percentage White Other',
+            group: 'Social - Population - Ethnicity',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_eth_white_other',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Percentage': 'perc_white_other'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Percentage Multi-ethnic (White/Black African)',
+            group: 'Social - Population - Ethnicity',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_eth_multi_white_black_african',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Percentage': 'perc_multi_white_black_african'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Percentage Multi-ethnic (White/Black Caribbean)',
+            group: 'Social - Population - Ethnicity',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_eth_black_british_caribbean',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Percentage': 'perc_black_british_carribean'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Percentage Multi-ethnic (White/Asian)',
+            group: 'Social - Population - Ethnicity',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_eth_multi_white_asian',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Percentage': 'perc_multi_white_asian'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Percentage Multi-ethnic (White/Other)',
+            group: 'Social - Population - Ethnicity',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_eth_multi_other',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Percentage': 'perc_multi_other'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Percentage Asian British (Indian)',
+            group: 'Social - Population - Ethnicity',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_eth_asian_british_indian',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Percentage': 'perc_asian_british_indian'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Percentage Asian British (Pakistani)',
+            group: 'Social - Population - Ethnicity',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_eth_asian_british_pakistani',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Percentage': 'perc_asian_british_pakistani'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Percentage Asian British (Bangladeshi)',
+            group: 'Social - Population - Ethnicity',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_eth_asian_british_bangladeshi',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Percentage': 'perc_asian_british_bangladeshi'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Percentage Asian British (Chinese)',
+            group: 'Social - Population - Ethnicity',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_eth_asian_british_chinese',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Percentage': 'perc_asian_british_chinese'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Percentage Asian British (Other)',
+            group: 'Social - Population - Ethnicity',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_eth_asian_british_other',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Percentage': 'perc_asian_british_other'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Percentage Black British (African)',
+            group: 'Social - Population - Ethnicity',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_eth_black_british_african',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Percentage': 'perc_black_british_african'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Percentage Black British (Caribbean)',
+            group: 'Social - Population - Ethnicity',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_eth_black_british_caribbean',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Percentage': 'perc_black_british_carribean'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Percentage Black British (Other)',
+            group: 'Social - Population - Ethnicity',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_eth_black_british_other',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Percentage': 'perc_black_british_other'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Percentage Arab',
+            group: 'Social - Population - Ethnicity',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_eth_arab',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Percentage': 'perc_arab'
+                    }
+                }
+            },
+        },
+        {
+            key: 'Percentage Other',
+            group: 'Social - Population - Ethnicity',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'planning_sustainability_appraisal:mv_eth_other',
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Percentage': 'perc_other'
+                    }
+                }
+            },
         }
 
     ]
