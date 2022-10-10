2e6b21d10d0bad41cefbcdbbe58fd57a4e208826
Jon Chiles <jonchiles@yahoo.com>
2022-10-10 14:35:56 +0100

    chore(test map): live prod link

--- Configuration/story-maps-test/index.js
+++ Configuration/story-maps-test/index.js
@@ -5,7 +5,7 @@ const groupOneTitle = 'Town Centre 1 - Stockport Town Centre Principles'
 const groupTwoTitle = 'Town Centre 2 - The Town Centre Character Areas'
 const groupThreeTitle = 'Centres 2 - Centres Designation Hierarchy'
 
-var stageData = fetch('http://localplan.stockport.gov.uk/RetrieveData/ChapterStages?chapterName=story-maps-test')
+var stageData = fetch('https://localplan.stockport.gov.uk/RetrieveData/ChapterStages?chapterName=story-maps-test')
 
 export default {
     Map: {
