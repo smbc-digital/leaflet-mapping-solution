4c5307b5c0d98f1785295dc0d9341d20a68673c6
Jaime Scowcroft <jaime.scowcroft@stockport.gov.uk>
2022-10-19 10:31:50 +0100

    warm spaces wms test

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 const { DefinePlugin } = require('webpack')
 
-const solution = 'warm-spaces'
+const solution = 'warm-spaces-new'
 
 module.exports = (env, argv, t) => (
     {
