07f1cf9af4067324e3a8e20f0c948250e315fbbe
TomWood116 <thomas.wood@stockport.gov.uk>
2022-11-18 11:38:20 +0000

    Updated pharmacies popup

--- Configuration/pharmacies/Popups/index.js
+++ Configuration/pharmacies/Popups/index.js
@@ -1,5 +1,34 @@
 const pharmacyPopup = (properties) => {
+
+  var services = properties.gm_minor_ailment_service === 'Yes' ? 'Minor Ailment Service' : ''
+  if (services === '') {
+    services = properties.emergency_hormonal_contraception === 'Yes' ? 'Emergency Hormonal Contraception' : ''
+  } else {
+    services += properties.emergency_hormonal_contraception === 'Yes' ? ', <br>Emergency Hormonal Contraception' : ''
+  }
   
+  if (services === '') {
+    services = properties.needle_exchange_services === 'Yes' ? 'Needle Exchange' : ''
+  } else {
+    services += properties.needle_exchange_services === 'Yes' ? ', <br>Needle Exchange' : ''
+  }
+
+  if (services === '') {
+    services = properties.stop_smoking_advanced === 'Yes' ? 'Stop Smoking Advanced Service' : ''
+  } else {
+    services += properties.stop_smoking_advanced === 'Yes' ? ', <br>Stop Smoking Advanced Service' : ''
+  }
+
+  if (services === '') {
+    services = properties.supervised_consumption_services === 'Yes' ? 'Supervised Consumption' : ''
+  } else {
+    services += properties.supervised_consumption_services === 'Yes' ? ', <br>Supervised Consumption' : ''
+  }
+
+  if (services === '') {
+    services = 'N/A'
+  }
+
   return `<div class="smbc-map__item">
   <div class="smbc-map__item__header__block">  
     <i class="fa fa-plus-square smbc-map__item__header__block__icon" aria-hidden="true"></i>
@@ -7,13 +36,8 @@ const pharmacyPopup = (properties) => {
   </div>
     <div class="smbc-map__item__body">
       <p>Name: ${properties.pharmacy_name}</p>
-      <p>Type: ${properties.pharmacy_type}</p>
       <p>Address: ${properties.address}, ${properties.postcode}</p>
-      <p>Minor Ailment Service?: ${properties.gm_minor_ailment_service}</p>
-      <p>Emergency Hormonal Contraception Service?: ${properties.emergency_hormonal_contraception}</p>
-      <p>Needle Exchange Service?: ${properties.needle_exchange_services}</p>
-      <p>Stop Smoking Advanced Service?: ${properties.stop_smoking_advanced}</p>
-      <p>Supervised Consumption Services?: ${properties.supervised_consumption_services}</p>
+      <p>Services: <br>${services}</p>
     </div>
   </div>`
 
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 const { DefinePlugin } = require('webpack')
 
-const solution = 'political-boundaries'
+const solution = 'pharmacies'
 
 module.exports = (env, argv, t) => (
     {
