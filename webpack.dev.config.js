e7da29c7e1c4770a3067b854af4fd0f27d53f161
Stu <h.s.j.wilkes@gmail.com>
2022-12-19 07:35:34 +0000

    chore(MapConfig): update Tree Reporting map

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 const { DefinePlugin } = require('webpack')
 
-const solution = 'polling-district-review-internal'
+const solution = 'tree-reporting'
 
 module.exports = (env, argv, t) => (
     {
