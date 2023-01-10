220f0e7a1555a5b9607ed35638c131c153f10903
TomWood116 <thomas.wood@stockport.gov.uk>
2023-01-10 16:50:09 +0000

    Updated community transport to WMS

--- Configuration/community-transport/index.js
+++ Configuration/community-transport/index.js
@@ -1,13 +1,6 @@
 import Leaflet from 'leaflet'
-import { busStopsPopup, prowPopup, pathPopup, taxiranksPopup, carparksPopup,  speedLimitPopup, leases_Popup, land_Ownership_Popup,  troMovingPopup } from './Popups'
-import { busStopsStyle, prowStyle, pathStyle, taxiranksStyle, carparksStyle,  speedLimitStyle, Leasesstyle, LandOwnershipstyle, adoptedhighwayStyle,  troMovingStyle} from './Styles'
-
-const groupOneTitle = 'Land and Assets'
-const groupTwoTitle = 'Political'
-const groupThreeTitle = 'Transport'
-const groupFourTitle = 'Highways Legal'
-const groupFiveTitle = 'Other'
-
+import {} from './Popups'
+import {} from './Styles'
 
 export default {
     Map: {},
@@ -32,89 +25,55 @@ export default {
         },
         
         {
+            key: 'Major Roads',
+            group: 'Highways Legal',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'highways:oshighways_major_roads',
+                key: {align: 'below'},
+                popup: { 
+                    icon: 'fa fa-square-o',
+                    body: {
+                        'Road': 'road_name',
+                    }
+                }
+            },
+        },
+        {
             key: 'Adopted Highway',
-            group: groupFourTitle,
-            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=con29:2_1a&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
+            group: 'Highways Legal',
+            url: 'wms',
+            visibleByDefault: false,
             layerOptions: {
-                maxZoom: 15,
-                style: adoptedhighwayStyle
+                layers: 'land_ownership:adopted_highways',
+                minZoom: 15,
             },
-            visibleByDefault: false
         },
-
-
-        {
-            key: 'Council Owned Land',
-            group: groupOneTitle,
-            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=land_ownership:council_owned_land&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
-            layerOptions: {
-                onEachFeature: land_Ownership_Popup,
-                style: LandOwnershipstyle,
-                key: {
-                    type: 'array',
-                    graphic: [
-                      { text: 'Corporate', style: 'fill:#ffff00;fill-opacity:0.5;' },
-                      { text: 'Education', style: 'fill:#ffaf5f;fill-opacity:0.5;' },
-                      { text: 'Greenspace', style: 'fill:#55ff55;fill-opacity:0.5;' },
-                      { text: 'Highways', style: 'fill:#3255ff;fill-opacity:0.5;' },
-                      { text: 'Social Services', style: 'fill:#c88cff;fill-opacity:0.5;' },
-                      { text: 'Stockport Homes', style: 'fill:#d70000;fill-opacity:0.5;' },
-                      { text: 'Strategic Housing', style: 'fill:#15ebf6;fill-opacity:0.5;' },
-                      { text: 'Other', style: 'fill:#000000;fill-opacity:0.5;' }
-                    ]
-                  }    
-            },
-            visibleByDefault: false
-        },
-
-        {
-            key: 'Leases',
-            group: groupOneTitle,
-            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=land_ownership:leases&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
-            layerOptions: {
-                onEachFeature: leases_Popup,
-                style: Leasesstyle
-            },
-            visibleByDefault: false
-        },
-
         {
             key: 'Public Rights of Way',
-            group: groupFourTitle,
-            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=highways:public_rights_of_way&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
+            group: 'Highways Legal',
+            url: 'wms',
+            visibleByDefault: false,
             layerOptions: {
-                onEachFeature: prowPopup,
-                style: prowStyle,
-            key: {
-                type: 'pline'
-            }
+                layers: 'highways:public_rights_of_way',
+                popup: {
+                    icon: 'fa fa-map-signs',
+                    body: {
+                      'PROW Number': 'row',
+                      'Type': 'type'
+                    }
+                  }
             },
-            visibleByDefault: false
         },
-
-        {
-            key: 'Paths Through Parks',
-            group: groupOneTitle,
-            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=transport:non_prow_paths_in_greenspace&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
-            layerOptions: {
-                onEachFeature: pathPopup,
-                style: pathStyle,
-            key: {
-                type: 'pline'
-            }
-            },
-            visibleByDefault: false
-        },
-
         {
             key: 'Traffic Regulation Order (moving)',
-            group: groupFourTitle,
+            group: 'Highways Legal',
             url: 'wms',
+            visibleByDefault: false,
             layerOptions: {
                 layers: 'highways:moving_tro_all',
-                key: {
-                    align: 'below'
-                  },
+                key: {align: 'below'},
                 popup: { 
                     icon: 'fa fa-car',
                     body: {
@@ -123,45 +82,104 @@ export default {
                     }
                 }
             },
-            visibleByDefault: false
         },
-
         {
             key: 'Traffic Regulation Order (static)',
-            group: groupFourTitle,
+            group: 'Highways Legal',
             url: 'wms',
+            visibleByDefault: false,
             layerOptions: {
                 layers: 'highways:static_tro_live',
-                key: {
-                    align: 'below'
-                  },
+                key: {align: 'below'},
                 popup: { 
                     icon: 'fa fa-car',
                     body: {
                         'Type': 'restriction',
                     }
                 }
-            },
-            visibleByDefault: false
+            }
         },
 
+        {
+            key: 'Council Owned Land',
+            group: 'Land and Assets',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'land_ownership:council_owned_land',
+                key: {align: 'below'},
+                popup: { 
+                    icon: 'fa fa-image',
+                    body: {
+                        'Name': 'property_name',
+                        'Committee': 'committee_new',
+                    }
+                }
+            }
+        },
+        {
+            key: 'Leases',
+            group: 'Land and Assets',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'land_ownership:leases',
+                popup: { 
+                    icon: 'fa fa-file-text',
+                    body: {
+                        'Committee': 'description',
+                    }
+                }
+            }
+        },
+        {
+            key: 'Paths Through Parks',
+            group: 'Land and Assets',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'transport:non_prow_paths_in_greenspace',
+            }
+        },
+        {
+            key: 'Structures',
+            group: 'Land and Assets',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'highway_assets:mv_structures_filtered',
+                key: {align: 'below'},
+                popup: { 
+                    icon: 'fas fa-archway',
+                    body: {
+                        'Name': 'structure_name',
+                        'Type': 'structure_type',
+                        'Owner': 'structue_owner',
+                    }
+                }
+            },
+        },
 
         {
             key: 'Taxi Ranks',
-            group: groupThreeTitle,
-            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=highways:taxi_ranks&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
+            group: 'Transport',
+            url: 'wms',
+            visibleByDefault: false,
             layerOptions: {
-                maxZoom: 2,
-                onEachFeature: taxiranksPopup,
-                style: taxiranksStyle,
+                layers: 'highways:taxi_ranks',
+                popup: { 
+                    icon: 'fa fa-taxi',
+                    body: {
+                        'Location': 'desc'
+                    }
+                }
             },
-            visibleByDefault: false
         },
-
         {
             key: 'Electric Vehicle Charging',
-            group: groupThreeTitle,
+            group: 'Transport',
             url: 'wms',
+            visibleByDefault: false,
             layerOptions: {
                 layers: 'transport:ev_charge_locations',
                 popup: { 
@@ -172,78 +190,54 @@ export default {
                     }
                 }
             },
-            visibleByDefault: false
         },
-
         {
             key: 'Car Parks',
-            group: groupThreeTitle,
-            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=highways:car_parks_points&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
+            group: 'Transport',
+            url: 'wms',
+            visibleByDefault: false,
             layerOptions: {
-                onEachFeature: carparksPopup,
-                maxZoom: 2,
-                style: carparksStyle,
-                pointToLayer: (feature, latlng) => {
-                    return Leaflet.circleMarker(latlng)
-                },
-                },
-                visibleByDefault: false
-            },
-
-            {
-                key: 'Bus Stops',
-                group: groupThreeTitle,
-                url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=transport:bus_stops&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
-                layerOptions: {
-                    onEachFeature: busStopsPopup,
-                    maxZoom: 15,
-                    style: busStopsStyle,
-                    pointToLayer: (feature, latlng) => {
-                        return Leaflet.circleMarker(latlng)
-                    },
-                },
-                    visibleByDefault: false
-            },
-    
-            {
-                key: 'Crossing Locations',
-                group: groupThreeTitle,
-                url: 'wms',
-                layerOptions: {
-                    layers: 'transport:crossings',
-                    key: {
-                        align: 'below'
-                      },
-                    popup: { 
-                        icon: 'fas fa-walking',
-                        body: {
-                            'Type': 'type',
-                            'Source': 'data_source',
-                        }
+                layers: 'highways:car_parks_points',
+                popup: { 
+                    icon: 'fab fa-product-hunt',
+                    body: {
+                        'Name': 'name',
+                        'Spaces': 'total_capacity'
                     }
-                },
-                visibleByDefault: false
+                }
             },
-
-            {
-                key: 'Structures',
-                group: groupOneTitle,
-                url: 'wms',
-                layerOptions: {
-                    layers: 'highway_assets:mv_structures_filtered',
-                    key: {
-                        align: 'below'
-                      },
-                    popup: { 
-                        icon: 'fas fa-archway',
-                        body: {
-                            'Name': 'structure_name',
-                            'Type': 'structure_type',
-                            'Owner': 'structue_owner',
-                        }
+        },
+        {
+            key: 'Bus Stops',
+            group: 'Transport',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'transport:bus_stops',
+                popup: { 
+                    icon: 'fas fa-bus-alt',
+                    body: {
+                        'Frequency': 'frequency'
                     }
-                },
-                visibleByDefault: false
-            }
+                }
+            },
+        },
+        {
+            key: 'Crossing Locations',
+            group: 'Transport',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'transport:crossings',
+                key: {align: 'below'},
+                popup: { 
+                    icon: 'fas fa-walking',
+                    body: {
+                        'Type': 'type',
+                        'Source': 'data_source',
+                    }
+                }
+            },
+        }
     ]
 }
