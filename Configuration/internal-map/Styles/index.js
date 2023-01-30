ec6bdba48d2cd27871727c4ff6b946f9b8ceb0b7
Jaime Scowcroft <jaime.scowcroft@stockport.gov.uk>
2023-01-30 18:15:13 +0000

    test map for internal only layers

---
+++ Configuration/internal-map/Styles/index.js
@@ -0,0 +1,11 @@
+const Article4_1_direction_style = {
+    color: '#cab2d6',
+    weight: 2,
+    opacity: 1,
+    fillColor: '#cab2d6',
+    fillOpacity: 0.5
+    }
+
+export {
+    Article4_1_direction_style
+}
---
+++ Configuration/internal-map/index.js
@@ -0,0 +1,33 @@
+import Leaflet from 'leaflet'
+import {} from './Popups'
+import {Article4_1_direction_style} from './Styles'
+
+const Configuration = {
+    Map: {
+        
+
+    },
+
+    Tiles: {
+        Token: '3G26OzBg7XRROryDwG1o1CZRmIx66ulo'
+    }, 
+
+    DynamicData: 
+    [
+
+        
+
+        {
+            key: '500m',
+            url: 'http://scnpostgres:8080/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=test:borough_500m_buffer&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
+            layerOptions: {
+                
+                
+                style: Article4_1_direction_style
+            },
+        },
+        
+    ],
+}
+
+export default Configuration
