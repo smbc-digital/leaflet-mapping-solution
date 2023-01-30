9bf4aa89f79259747ce23cc6bfce19ec06537625
TomWood116 <thomas.wood@stockport.gov.uk>
2023-01-30 14:17:09 +0000

    Updated TPO web map to WMS

--- Configuration/tree-preservation-orders/Popups/index.js
+++ Configuration/tree-preservation-orders/Popups/index.js
@@ -1,37 +1,3 @@
-const tpoPopup = (feature, layer) => {
-  
-  const content = `<div class="smbc-map__item">
-    <div class="smbc-map__item__header__block">
-      <i class="fas fa-tree smbc-map__item__header__block__icon" aria-hidden="true"></i>
-      <span class="smbc-map__item__header__block__title">Tree Preservation Order</span>
-    </div>
-    <div class="smbc-map__item__body">
-      <p>Reference: ${feature.properties.tpo_number}</p>
-      <p>Tree Number: ${feature.properties.tree_number}</p>
-      <p>Name: ${feature.properties.tpo_name}</p>
-      <p>Species: ${feature.properties.tree_species}</p>
-      <p>Status: ${feature.properties.status}</p>
-    </div>
-  </div>`
- 
-  layer.bindPopup(content)
- }
-
- const conservationPopup = (feature, layer) => {
-  
-  const content = `<div class="smbc-map__item">
-    <div class="smbc-map__item__header__block">
-      <i class="fa fa-home smbc-map__item__header__block__icon" aria-hidden="true"></i>
-      <span class="smbc-map__item__header__block__title">Conservation Area</span>
-    </div>
-    <div class="smbc-map__item__body">
-      <p>Name: ${feature.properties.cons_area}</p>
-    </div>
-  </div>`
- 
-  layer.bindPopup(content)
- }
-
  const planappPopup = (feature, layer) => {
   
   const content = `<div class="smbc-map__item">
@@ -52,8 +18,4 @@ const tpoPopup = (feature, layer) => {
   layer.bindPopup(content)
  }
 
-export {
-  tpoPopup,
-  conservationPopup,
-  planappPopup
-}
+export {}
--- Configuration/tree-preservation-orders/index.js
+++ Configuration/tree-preservation-orders/index.js
@@ -1,28 +1,33 @@
 import Leaflet from 'leaflet'
-import {tpoPopup, conservationPopup, planappPopup} from './Popups'
-import {tpoStyle,revokedtpoStyle,conservationStyle,planappStyle} from './Styles'
+import {} from './Popups'
+import {} from './Styles'
 
 const Configuration = {
-    Map: {
-
-    },
-
-    Tiles: {
-        Token: '3G26OzBg7XRROryDwG1o1CZRmIx66ulo'
-    }, 
+    Map: {},
+    Tiles: {Token: '3G26OzBg7XRROryDwG1o1CZRmIx66ulo'},
+    LayerControlOptions: { keyGraphic: true, groupCheckboxes: true },
 
     DynamicData: 
     [
         {
             key: 'Tree Preservation Orders',
-            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=trees:tpo_merged&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
+            url: 'wms',
             layerOptions: {
-                onEachFeature: tpoPopup,
-                maxZoom: 2,
-                style: tpoStyle
+                layers: 'trees:tpo_merged',
+                minZoom: 2,
+                popup: {
+                    icon: 'fas fa-tree',
+                    body: {
+                        'Reference': 'tpo_number',
+                        'Tree Number': 'tree_number',
+                        'Name': 'tpo_name',
+                        'Species': 'tree_species',
+                        'Status': 'status',
+                    }
+                  }
             },
+            visibleByDefault: true
         },
-
        /* {
             key: 'TPO - Proposed',
             url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=trees:tpo_proposed&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
@@ -32,55 +37,97 @@ const Configuration = {
                 style: proposedtpoStyle
             },
         },*/
-
         {
-            key: 'TPO - Revoked',
-            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=trees:tpo_revoked&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
+            key: 'Revoked Tree Preservation Orders',
+            url: 'wms',
             layerOptions: {
-                onEachFeature: tpoPopup,
-                maxZoom: 2,
-                style: revokedtpoStyle
+                layers: 'trees:tpo_revoked',
+                minZoom: 2,
+                popup: {
+                    icon: 'fas fa-tree',
+                    body: {
+                        'Reference': 'tpo_number',
+                        'Tree Number': 'tree_number',
+                        'Name': 'tpo_name',
+                        'Species': 'tree_species',
+                        'Status': 'status',
+                    }
+                  }
             },
+            visibleByDefault: true
         },
-
         {
             key: 'Conservation Areas',
-            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=heritage:conservation_area&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
+            url: 'wms',
             layerOptions: {
-                onEachFeature: conservationPopup,
-                maxZoom: 2,
-                style: conservationStyle
+                layers: 'heritage:conservation_area',
+                minZoom: 2,
+                popup: {
+                    icon: 'fa fa-home',
+                    body: {
+                        'Name': 'cons_area',
+                    }
+                  }
             },
+            visibleByDefault: true
         },
-
         {
-            key: 'Tree Planning App Last Month',
-            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=tree_plan_apps_1month&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
+            key: 'Tree Planning App. (Last Month)',
+            url: 'wms',
             layerOptions: {
-                onEachFeature: planappPopup,
-                maxZoom: 2,
-                style: planappStyle
+                layers: 'trees:tree_plan_apps_1month',
+                minZoom: 2,
+                popup: {
+                    icon: 'far fa-map',
+                    body: {
+                        'Reference': 'refval',
+                        'Proposal': 'proposal',
+                        'Address': 'address',
+                        'Recieved Date': 'received_date',
+                        'Decision': 'decision',
+                        'Decision Date': 'decision_date'
+                    }
+                  }
             },
+            visibleByDefault: true
         },
-
         {
-            key: 'Tree Planning App Last Year',
-            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=trees:tree_plan_apps_1year&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
+            key: 'Tree Planning App. (Last Year)',
+            url: 'wms',
             layerOptions: {
-                onEachFeature: planappPopup,
-                maxZoom: 2,
-                style: planappStyle
+                layers: 'trees:tree_plan_apps_1year',
+                minZoom: 2,
+                popup: {
+                    icon: 'far fa-map',
+                    body: {
+                        'Reference': 'refval',
+                        'Proposal': 'proposal',
+                        'Address': 'address',
+                        'Recieved Date': 'received_date',
+                        'Decision': 'decision',
+                        'Decision Date': 'decision_date'
+                    }
+                  }
             },
             visibleByDefault: false
         },
-
         {
-            key: 'Tree Planning App Last 3 Years',
-            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=trees:tree_plan_apps_3years&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
+            key: 'Tree Planning App. (Last 3 Years)',
+            url: 'wms',
             layerOptions: {
-                onEachFeature: planappPopup,
-                maxZoom: 2,
-                style: planappStyle
+                layers: 'trees:tree_plan_apps_3years',
+                minZoom: 2,
+                popup: {
+                    icon: 'far fa-map',
+                    body: {
+                        'Reference': 'refval',
+                        'Proposal': 'proposal',
+                        'Address': 'address',
+                        'Recieved Date': 'received_date',
+                        'Decision': 'decision',
+                        'Decision Date': 'decision_date'
+                    }
+                  }
             },
             visibleByDefault: false
         },
