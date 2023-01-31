94d34c0a48b7ffdc6b8ed14f9c7157751cb5d6cf
TomWood116 <thomas.wood@stockport.gov.uk>
2023-01-31 09:18:46 +0000

    Update to Leisure Centres map

--- Configuration/leisure-centres/Popups/index.js
+++ Configuration/leisure-centres/Popups/index.js
@@ -1,4 +1,4 @@
-const leisurecentresPopup = (feature, layer) => {
+/* const leisurecentresPopup = (feature, layer) => {
   
   const content = `<div class="smbc-map__item">
 <div class="smbc-map__item__header__block">
@@ -14,6 +14,21 @@ const leisurecentresPopup = (feature, layer) => {
 </div>`
 
   layer.bindPopup(content)
+} */
+
+const leisurecentresPopup = (properties) => {
+  return `<div class="smbc-map__item">
+    <div class="smbc-map__item__header__block">
+      <i class="fa fa-heart smbc-map__item__header__block__icon" aria-hidden="true"></i>
+      <span class="smbc-map__item__header__block__title">Library</span>
+      </div>
+      <div class="smbc-map__item__body">
+      <a href="${properties.url}" target="_blank">${properties.centre_name}</a>
+      <p>Address: ${properties.address}</p>
+      <p>Tel: ${properties.telephone}</p>
+      <a href="mailto:${properties.email}" target="_blank">Email Leisure Centre</a>
+    </div>
+  </div>`
 }
 
 export {
--- Configuration/leisure-centres/index.js
+++ Configuration/leisure-centres/index.js
@@ -1,33 +1,24 @@
 import Leaflet from 'leaflet'
 import {leisurecentresPopup} from './Popups'
-import {leisurecentresStyle} from './Styles'
+import {} from './Styles'
 
 const Configuration = {
-    Map: {
-
-    },
-
-    Tiles: {
-        Token: '3G26OzBg7XRROryDwG1o1CZRmIx66ulo'
-    }, 
+    Map: { },
+    Tiles: {Token: '3G26OzBg7XRROryDwG1o1CZRmIx66ulo'},
+    LayerControlOptions: { keyGraphic: true, groupCheckboxes: true },
 
     DynamicData: 
     [
-
         {
-            key: 'Leisure Centres',
-            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=buildings_and_land:leisure_centres&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
+            key: 'Leisure Centres (WMS)',
+            url: 'wms',
             layerOptions: {
-                onEachFeature: leisurecentresPopup,
-                maxZoom: 2,
-                style: leisurecentresStyle,
-                pointToLayer: (feature, latlng) => {
-                    return Leaflet.circleMarker(latlng)
-                },
-
+                layers: 'buildings_and_land:leisure_centres',
+                visibleByDefault: true,
+                minZoom: 2,
+                popup: leisurecentresPopup
             },
-        },
-        
+        },        
     ],
 }
 
