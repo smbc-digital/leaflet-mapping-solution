5332fe0de938029d82591d038930311b8f15fa31
TomWood116 <thomas.wood@stockport.gov.uk>
2023-07-27 12:58:19 +0100

    Created A34 web map

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 const { DefinePlugin } = require('webpack')
 
-const solution = 'planning-sustainability-appraisal'
+const solution = 'a34-improvement-plan'
 
 module.exports = (env, argv, t) => (
     {
