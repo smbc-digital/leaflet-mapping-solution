db2b167ff9fb25422f5528311a2923e80fc4a2f9
Jaime Scowcroft <jaime.scowcroft@stockport.gov.uk>
2023-08-10 14:39:43 +0100

    group Stockport Homes

--- Configuration/sh-ownership/index.js
+++ Configuration/sh-ownership/index.js
@@ -9,8 +9,9 @@ const Configuration = {
     DynamicData: 
     [
         {
-            key: 'Stockport Homes - Stock List',
+            key: 'Stock List',
             url: 'wms',
+            group: 'Stockport Homes',
             displayInOverlay: true,
             visibleByDefault: false,
             layerOptions: {
@@ -29,76 +30,11 @@ const Configuration = {
             },
         },
         {
-            key: 'Great Places',
-            url: 'wms',
-            displayInOverlay: true,
-            visibleByDefault: false,
-            group: 'RSL',
-            layerOptions: {
-                layers: 'stockport_homes:rsl_great_places',
-                key: {align: 'below'}
-            },
-        },
-        {
-            key: 'Guinness Partnership',
-            url: 'wms',
-            displayInOverlay: true,
-            visibleByDefault: false,
-            group: 'RSL',
-            layerOptions: {
-                layers: 'stockport_homes:rsl_guinness',
-                key: {align: 'below'}
-            },
-        },
-        {
-            key: 'Johnnie Johnson Housing',
-            url: 'wms',
-            displayInOverlay: true,
-            visibleByDefault: false,
-            group: 'RSL',
-            layerOptions: {
-                layers: 'stockport_homes:rsl_jjht',
-                key: {align: 'below'}
-            },
-        },
-        {
-            key: 'Mosscare St Vincents',
-            url: 'wms',
-            displayInOverlay: true,
-            visibleByDefault: false,
-            group: 'RSL',
-            layerOptions: {
-                layers: 'stockport_homes:rsl_msv',
-                key: {align: 'below'}
-            },
-        },
-        {
-            key: 'Onward',
-            url: 'wms',
-            displayInOverlay: true,
-            visibleByDefault: false,
-            group: 'RSL',
-            layerOptions: {
-                layers: 'stockport_homes:rsl_onward',
-                key: {align: 'below'}
-            },
-        },
-        {
-            key: 'RSL - Your Housing',
-            url: 'wms',
-            displayInOverlay: true,
-            visibleByDefault: false,
-            group: 'RSL',
-            layerOptions: {
-                layers: 'stockport_homes:rsl_your_housing',
-                key: {align: 'below'}
-            },
-        },
-        {
-            key: 'Stockport Homes - Caretaker Sites',
+            key: 'Caretaker Sites',
             url: 'wms',
             displayInOverlay: true,
             visibleByDefault: true,
+            group: 'Stockport Homes',
             layerOptions: {
                 layers: 'stockport_homes:sh_caretaker_sites',
                 key: {align: 'below'},
@@ -114,10 +50,77 @@ const Configuration = {
             },
         },
         {
+            key: 'Great Places',
+            url: 'wms',
+            displayInOverlay: true,
+            visibleByDefault: false,
+            group: 'Registered Social Landlords',
+            layerOptions: {
+                layers: 'stockport_homes:rsl_great_places',
+                key: {align: 'below'}
+            },
+        },
+        {
+            key: 'Guinness Partnership',
+            url: 'wms',
+            displayInOverlay: true,
+            visibleByDefault: false,
+            group: 'Registered Social Landlords',
+            layerOptions: {
+                layers: 'stockport_homes:rsl_guinness',
+                key: {align: 'below'}
+            },
+        },
+        {
+            key: 'Johnnie Johnson Housing',
+            url: 'wms',
+            displayInOverlay: true,
+            visibleByDefault: false,
+            group: 'Registered Social Landlords',
+            layerOptions: {
+                layers: 'stockport_homes:rsl_jjht',
+                key: {align: 'below'}
+            },
+        },
+        {
+            key: 'Mosscare St Vincents',
+            url: 'wms',
+            displayInOverlay: true,
+            visibleByDefault: false,
+            group: 'Registered Social Landlords',
+            layerOptions: {
+                layers: 'stockport_homes:rsl_msv',
+                key: {align: 'below'}
+            },
+        },
+        {
+            key: 'Onward',
+            url: 'wms',
+            displayInOverlay: true,
+            visibleByDefault: false,
+            group: 'Registered Social Landlords',
+            layerOptions: {
+                layers: 'stockport_homes:rsl_onward',
+                key: {align: 'below'}
+            },
+        },
+        {
+            key: 'RSL - Your Housing',
+            url: 'wms',
+            displayInOverlay: true,
+            visibleByDefault: false,
+            group: 'Registered Social Landlords',
+            layerOptions: {
+                layers: 'stockport_homes:rsl_your_housing',
+                key: {align: 'below'}
+            },
+        },
+        {
             key: 'Council Owned Land',
             url: 'wms',
             displayInOverlay: true,
             visibleByDefault: false,
+            group: 'Land Ownership',
             layerOptions: {
                 layers: 'land_ownership:council_owned_land',
                 key: {align: 'below'},
