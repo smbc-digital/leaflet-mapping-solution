62c0b67b0fe5179cc713ee84516f20e7ef70b14f
TomWood116 <thomas.wood@stockport.gov.uk>
2023-08-15 13:12:12 +0100

    Update to A34 popup

--- Configuration/a34-improvement-plan/Popups/index.js
+++ Configuration/a34-improvement-plan/Popups/index.js
@@ -1,16 +1,37 @@
 const a34Popup = (properties) => {
-  return `<div class="smbc-map__item">
+  let popupContent = `<div class="smbc-map__item">
     <div class="smbc-map__item__header__block">
       <i class="fa fa-car smbc-map__item__header__block__icon" aria-hidden="true"></i>
       <span class="smbc-map__item__header__block__title">A34 Improvement Areas</span>
     </div>
-      <div class="smbc-map__item__body">
-        <p>Scheme Number: ${properties.scheme_no}</p>
-        <p>Project Title: ${properties.project_title}</p>
-        <p>Construction Status: ${properties.timescale}</p>
-        <p>${properties.pdf_link}</p>
-    </div>
-  </div>`
-}
+    <div class="smbc-map__item__body">
+      <p>Scheme Number: ${properties.scheme_no}</p>
+      <p>Project Title: ${properties.project_title}</p>
+      <p>Construction Status: ${properties.timescale}</p>
+      <br>PDF Documents:
+      <p>${properties.dev_pdf_link}</p>
+      <p>${properties.con_pdf_link_1}</p>`;
+      
+  if (properties.con_pdf_link_2) {
+    popupContent += `<p>${properties.con_pdf_link_2}</p>`;
+  }
 
-export {a34Popup}
+  if (properties.con_pdf_link_3) {
+    popupContent += `<p>${properties.con_pdf_link_3}</p>`;
+  }
+
+  if (properties.con_pdf_link_4) {
+    popupContent += `<p>${properties.con_pdf_link_4}</p>`;
+  }
+
+  if (properties.con_pdf_link_5) {
+    popupContent += `<p>${properties.con_pdf_link_5}</p>`;
+  }
+
+  popupContent += `</div>
+  </div>`;
+  
+  return popupContent;
+};
+
+export {a34Popup};
