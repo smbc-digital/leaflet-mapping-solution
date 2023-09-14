12c22c9077815f59832dac1773546a187192239b
Jaime Scowcroft <jaime.scowcroft@stockport.gov.uk>
2023-09-14 12:57:14 +0100

    update internal-map to include WMS layer

--- Configuration/internal-map/index.js
+++ Configuration/internal-map/index.js
@@ -11,14 +11,14 @@ const Configuration = {
     Tiles: {
         Token: '3G26OzBg7XRROryDwG1o1CZRmIx66ulo'
     }, 
-
+    LayerControlOptions: { keyGraphic: true, groupCheckboxes: true },
     DynamicData: 
     [
 
         
 
         {
-            key: '500m',
+            key: 'Buffer WFS',
             url: 'https://scnpostgres:8443/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=base_maps:borough_500m_buffer&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
             layerOptions: {
                 
@@ -26,6 +26,13 @@ const Configuration = {
                 style: Article4_1_direction_style
             },
         },
+        {
+            key: 'Buffer WMS',
+            url: 'https://scnpostgres:8443/geoserver/wms?',
+            layerOptions: {
+                layers: 'base_maps:borough_500m_buffer'
+            },
+        }
         
     ],
 }
