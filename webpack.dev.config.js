7c3d1583e69bda53cbe62cae485f2c10afceb0b9
TomWood116 <thomas.wood@stockport.gov.uk>
2023-11-13 17:23:53 +0000

    Created healthy communities web map

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 const { DefinePlugin } = require('webpack')
 
-const solution = 'neighbourhood-transport-plan'
+const solution = 'healthy-communities'
 
 module.exports = (env, argv, t) => (
     {
