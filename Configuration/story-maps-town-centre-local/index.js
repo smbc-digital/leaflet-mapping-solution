f4630a3fb2d1f0da8f90393283f65d253f82ebf1
Jon Chiles <jonchiles@yahoo.com>
2023-12-01 15:20:27 +0000

    chore(mnp map): new map for spike

--- Configuration/story-maps-town-centre-local/index.js
+++ Configuration/story-maps-town-centre-local/index.js
@@ -6,7 +6,7 @@ const groupTwoTitle = 'Town Centre 2 - The Town Centre Character Areas'
 const groupThreeTitle = 'Centres 2 - Centres Designation Hierarchy'
 
 //var stageData = fetch('http://scnwebappsdev1.stockport.gov.uk:8080/RetrieveData/ChapterStages?chapterName=story-maps-town-centre')
-var stageData = fetch('https://localhost:44325/RetrieveData/ChapterStages?chapterName=story-maps-mnp-one')
+var stageData = fetch('https://localhost:44325/RetrieveData/ChapterStages?chapterName=story-maps-town-centre')
 
 export default {
     Map: {
