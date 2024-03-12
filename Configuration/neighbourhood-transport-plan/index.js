df9dbe39afa63c423b55199126e8c68737df97e4
Jaime Scowcroft <jaime.scowcroft@stockport.gov.uk>
2024-03-12 21:19:34 +0000

    Be EV popup NTP map

--- Configuration/neighbourhood-transport-plan/index.js
+++ Configuration/neighbourhood-transport-plan/index.js
@@ -331,7 +331,7 @@ export default {
             key: 'BE EV Rollout',
             group: 'Programmed Schemes',
             url: 'https://scnpostgres.stockport.gov.uk:8443/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=transport:be_ev_rollout_internal_use&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
-            visibleByDefault: true,
+            visibleByDefault: false,
             layerOptions: {
                 onEachFeature: BeEVPopup,
                 pointToLayer: (feature, latlng) => {
