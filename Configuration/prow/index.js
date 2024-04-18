922ffd19fc7414ba033c2b91e6a4e63e0287fd98
Jaime Scowcroft <jaime.scowcroft@stockport.gov.uk>
2024-04-18 16:55:25 +0100

    PROW - add layers for pending and stopping up orders

--- Configuration/prow/index.js
+++ Configuration/prow/index.js
@@ -7,9 +7,8 @@ const Configuration = {
 
     },
 
-    Tiles: {
-        Token: '3G26OzBg7XRROryDwG1o1CZRmIx66ulo'
-    }, 
+    Tiles: {Token: '3G26OzBg7XRROryDwG1o1CZRmIx66ulo'}, 
+    LayerControlOptions: { keyGraphic: true, groupCheckboxes: true },
 
     DynamicData: 
     [
@@ -17,6 +16,7 @@ const Configuration = {
         {
             key: 'Public Rights of Way',
             url: 'wms',
+            visibleByDefault: true,
             layerOptions: {
                 layers: 'highways:public_rights_of_way',
                 popup: {
@@ -24,6 +24,48 @@ const Configuration = {
                     body: {
                       'PROW Number': 'row',
                       'Type': 'type'
+                    }
+                  }
+            },
+        },
+
+        {
+            key: 'Pending PROW application',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'con29:2_3',
+                popup: {
+                    icon: 'fa fa-map-signs',
+                    body: {
+                      'PROW Number': 'row',
+                      'Type': 'type',
+                      'Status': 'status',
+                      'Length (m)': 'length_m',
+                        'Category': 'category',
+                        'Hierarchy': 'hierarchy',
+                      'Con29 Category': 'con_29_category'
+                    }
+                  }
+            },
+        },
+
+        {
+            key: 'Legal order to stop up, divert, alter or create a PROW',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'con29:2_4',
+                popup: {
+                    icon: 'fa fa-map-signs',
+                    body: {
+                        'PROW Number': 'row',
+                        'Type': 'type',
+                        'Status': 'status',
+                        'Length (m)': 'length_m',
+                        'Category': 'category',
+                        'Hierarchy': 'hierarchy',
+                        'Con29 Category': 'con_29_category'
                     }
                   }
             },
