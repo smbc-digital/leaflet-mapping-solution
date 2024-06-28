34e5fda52b23c72e7c6292db6ec00d62742a4566
Jaime Scowcroft <jaime.scowcroft@stockport.gov.uk>
2024-06-28 13:04:19 +0100

    smoke control change popup title

--- Configuration/smoke-control-defra/Popups/index.js
+++ Configuration/smoke-control-defra/Popups/index.js
@@ -1,8 +1,8 @@
 const smokecontrolareaPopup = (properties) => {
   return(`<div class="smbc-map__item">
     <div class="smbc-map__item__header__block">
-      <i class="fa fa-home smbc-map__item__header__block__icon" aria-hidden="true"></i>
-      <span class="smbc-map__item__header__block__title">Council Owned Property Assets</span>
+      <i class="fas fa-smog smbc-map__item__header__block__icon" aria-hidden="true"></i>
+      <span class="smbc-map__item__header__block__title">Smoke Control Order</span>
     </div>
     <div class="smbc-map__item__body">
         <p>Code: ${properties.code}</p>
