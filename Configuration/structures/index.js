48ebc93ca058dd4d9d7e4a54e04a313e39aa72a4
Jaime Scowcroft <jaime.scowcroft@stockport.gov.uk>
2024-09-16 08:58:09 +0100

    Structures - group types

--- Configuration/structures/index.js
+++ Configuration/structures/index.js
@@ -1,32 +1,107 @@
 import Leaflet from 'leaflet'
-import {carparksPopup} from './Popups'
-import {carparksStyle} from './Styles'
+import {structuresPopup} from './Popups'
+
 
 const Configuration = {
-    Map: {
-
-    },
-
-    Tiles: {
-        Token: '3G26OzBg7XRROryDwG1o1CZRmIx66ulo'
-    }, 
-
+    Map: {},
+    Tiles: {Token: '3G26OzBg7XRROryDwG1o1CZRmIx66ulo'}, 
+    LayerControlOptions: { keyGraphic: true, groupCheckboxes: true },
     DynamicData: 
     [
+        
         {
-            key: 'Structures',
+            key: 'Bridge',
+            group: 'Structures',
             url: 'wms',
             layerOptions: {
-                layers: 'highway_assets:structures',
+                layers: 'highway_assets:structure_bridge',
                 minZoom: 2,
-                popup: {
-                    icon: 'fa fa-cube',
-                    body: {
-                        Name: 'structure_name',
-                        Type: 'structure_type',
-                        Number: 'structure_number'
-                    }
-                  },
+                popup: structuresPopup
+                
+            },
+
+        },
+        {
+            key: 'Culvert',
+            group: 'Structures',
+            url: 'wms',
+            layerOptions: {
+                layers: 'highway_assets:structure_culvert',
+                minZoom: 2,
+                popup: structuresPopup
+                
+            },
+
+        },
+        {
+            key: 'Footbridge',
+            group: 'Structures',
+            url: 'wms',
+            layerOptions: {
+                layers: 'highway_assets:structure_footbridge',
+                minZoom: 2,
+                popup: structuresPopup
+                
+            },
+
+        },
+        {
+            key: 'Gantry',
+            group: 'Structures',
+            url: 'wms',
+            layerOptions: {
+                layers: 'highway_assets:structure_gantry',
+                minZoom: 2,
+                popup: structuresPopup
+                
+            },
+
+        },
+        {
+            key: 'Retaining Wall',
+            group: 'Structures',
+            url: 'wms',
+            layerOptions: {
+                layers: 'highway_assets:structure_retaining_wall',
+                minZoom: 2,
+                popup: structuresPopup
+                
+            },
+
+        },
+        {
+            key: 'Steps',
+            group: 'Structures',
+            url: 'wms',
+            layerOptions: {
+                layers: 'highway_assets:structure_steps',
+                minZoom: 2,
+                popup: structuresPopup
+                
+            },
+
+        },
+        {
+            key: 'Subway',
+            group: 'Structures',
+            url: 'wms',
+            layerOptions: {
+                layers: 'highway_assets:structure_subway',
+                minZoom: 2,
+                popup: structuresPopup
+                
+            },
+
+        },
+        {
+            key: 'Tunnel',
+            group: 'Structures',
+            url: 'wms',
+            layerOptions: {
+                layers: 'highway_assets:structure_tunnel',
+                minZoom: 2,
+                popup: structuresPopup
+                
             },
 
         },
