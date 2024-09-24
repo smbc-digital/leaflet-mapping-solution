42adb8aed0c420368337bc8f2af66b34bae19551
Jon Chiles <jonchiles@yahoo.com>
2024-09-24 13:54:37 +0100

    chore(configuration): deleted loads to reduce build time

---
+++ Configuration/structures-new/index.js
@@ -0,0 +1,36 @@
+import Leaflet from 'leaflet'
+import {carparksPopup} from './Popups'
+import {carparksStyle} from './Styles'
+
+const Configuration = {
+    Map: {
+
+    },
+
+    Tiles: {
+        Token: '3G26OzBg7XRROryDwG1o1CZRmIx66ulo'
+    }, 
+
+    DynamicData: 
+    [
+        {
+            key: 'Structures',
+            url: 'wms',
+            layerOptions: {
+                layers: 'highway_assets:structures_april2023',
+                minZoom: 2,
+                popup: {
+                    icon: 'fab fa-product-hunt',
+                    body: {
+                        Name: 'structure_name',
+                        
+                    }
+                  },
+            },
+
+        },
+        
+    ],
+}
+
+export default Configuration
--- Configuration/tree-preservation-orders/Styles/index.js
+++
@@ -1,9 +0,0 @@
-const planappStyle = {
-    color: '#8856a7',
-    weight: 1,
-    opacity: 1,
-    fillColor: '#8856a7',
-    fillOpacity: 0.5
-}
-
-export {}
