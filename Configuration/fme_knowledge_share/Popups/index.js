6011fb74143ca6914d7a98a3e1d82000cd3d1d02
Jon Chiles <jonchiles@yahoo.com>
2024-09-30 14:47:43 +0100

    chore(fontawesome): updated to 6.6.0 free

--- Configuration/fme_knowledge_share/Popups/index.js
+++ Configuration/fme_knowledge_share/Popups/index.js
@@ -1,7 +1,7 @@
  const poiPopup = (feature, layer) => {
   const content = `<div class="smbc-map__item">
 <div class="smbc-map__item__header__block">
-  <i class="fa fa-info-circle smbc-map__item__header__block__icon" aria-hidden="true"></i>
+  <i class="fa fa-bridge smbc-map__item__header__block__icon" aria-hidden="true"></i>
   <span class="smbc-map__item__header__block__title">Point of Interest</span>
 </div>
 <div class="smbc-map__item__body">
