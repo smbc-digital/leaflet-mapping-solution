706de609b0ebc821a1835ee8c6c0cb74ad706054
Jaime Scowcroft <jaime.scowcroft@stockport.gov.uk>
2025-04-03 17:08:22 +0100

    Car Parks new layer showing proposed charges

--- Configuration/car-parks/index.js
+++ Configuration/car-parks/index.js
@@ -10,6 +10,7 @@ const Configuration = {
     Tiles: {
         Token: '3G26OzBg7XRROryDwG1o1CZRmIx66ulo'
     }, 
+    LayerControlOptions: { keyGraphic: true, groupCheckboxes: true },
 
     DynamicData: 
     [
@@ -23,6 +24,32 @@ const Configuration = {
                     icon: 'fab fa-product-hunt',
                     body: {
                         Name: 'name',
+                        Postcode: 'post_code',
+                        'RingGo Zone': 'ringgo_zone',
+                        Spaces: 'total_capacity',
+                        'Disabled Spaces': 'disabled_spaces',
+                        'Parent and Child Spaces': 'parent_child_spaces',
+                        'Electric Vehicle Spaces': 'electric_vehicle_spaces',
+                        'Height Restriction': 'height_restriction_m',
+                        'Park Mark': 'park_mark'
+                    }
+                  },
+            },
+
+        },
+        {
+            key: 'Car Parks - Proposed',
+            url: 'wms',
+            visibleByDefault: false,
+            layerOptions: {
+                layers: 'highways:car_park_points_proposed',
+                key: {align: 'below'},
+                minZoom: 2,
+                popup: {
+                    icon: 'fab fa-product-hunt',
+                    body: {
+                        Name: 'name',
+                        Price: 'charging_category',
                         Postcode: 'post_code',
                         'RingGo Zone': 'ringgo_zone',
                         Spaces: 'total_capacity',
