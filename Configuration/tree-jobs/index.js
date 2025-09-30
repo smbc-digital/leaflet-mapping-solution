2c3d66e0658b519f2668645c91f02b8c0c208f57
Jaime Scowcroft <jaime.scowcroft@stockport.gov.uk>
2025-09-30 20:21:52 +0100

    tree jobs map

---
+++ Configuration/tree-jobs/index.js
@@ -0,0 +1,38 @@
+import Leaflet from 'leaflet'
+import {prowPopup} from './Popups'
+import {prowStyle} from './Styles'
+
+const Configuration = {
+    Map: {
+
+    },
+
+    Tiles: {Token: '3G26OzBg7XRROryDwG1o1CZRmIx66ulo'}, 
+    LayerControlOptions: { keyGraphic: true, groupCheckboxes: true },
+
+    DynamicData: 
+    [
+
+        {
+            key: 'Tree jobs',
+            url: 'wms',
+            visibleByDefault: true,
+            layerOptions: {
+                layers: 'biota:tree_jobs_outstanding',
+                popup: {
+                    icon: 'fa-solid fa-tree',
+                    body: {
+                      'Task': 'job_notes',
+                      'Status': 'job_status',
+                      'Area': 'feature_location',
+                      'Location': 'feature_notes',
+                      'Number': 'job_number',
+                    }
+                  }
+            },
+        }
+        
+    ],
+}
+
+export default Configuration
