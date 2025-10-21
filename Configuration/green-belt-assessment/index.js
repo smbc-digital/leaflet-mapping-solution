a484222f88b63ef9ad6c62490b517187ad69fce8
Jaime Scowcroft <jaime.scowcroft@stockport.gov.uk>
2025-10-21 12:19:14 +0100

    add grey belt to assessmet map

--- Configuration/green-belt-assessment/index.js
+++ Configuration/green-belt-assessment/index.js
@@ -9,7 +9,15 @@ export default {
             url: 'wms',
             layerOptions: {
                 layers: 'planning:green_belt_os',
-                styles: 'greenbelt - light'
+                styles: 'greenbelt - medium'
+            }
+        },
+        {
+            key: 'Grey Belt',
+            url: 'wms',
+            layerOptions: {
+                layers: 'planning_policy:grey_belt',
+                styles: 'grey_belt'
             }
         },
         {
