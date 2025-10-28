b5929bc89303aedaab58af1411b4299d05f85320
geospatial-dan <d.kinder02@gmail.com>
2025-10-28 16:36:05 +0000

    chore(maps): directory changes

--- Configuration/story-maps-our-homes/index.js
+++ Configuration/story-maps-our-homes/index.js
@@ -9,7 +9,7 @@ const groupThreeTitle = 'GM Joint Minerals Plan'
 //const groupFiveTitle = 'Environment 7 - Green Belt Development'
 //const groupSixTitle = 'Environment 8 - Protection of Agricultural land'
 
-var stageData = fetch(retrieveDataEndpoint(window.location.origin, 'story-maps-our-environment'))
+var stageData = fetch(retrieveDataEndpoint(window.location.origin, 'story-maps-our-homes'))
 
 export default {
     Map: {
