b04862985b8b09d06e80334c3cf87c2ededd5744
TomWood116 <thomas.wood@stockport.gov.uk>
2026-02-20 09:56:43 +0000

    Updates to housing web map
    Added unlicensed HMOs

--- Configuration/housing/index.js
+++ Configuration/housing/index.js
@@ -2,7 +2,7 @@ import { } from './Popups'
 import { } from './Styles'
 
 export default {
-    Map : {},
+    Map : {StartingZoom: 15, Latitude: 53.40588, Longitude: -2.15752},
     Tiles: {Token: '3G26OzBg7XRROryDwG1o1CZRmIx66ulo'},
     LayerControlOptions: { keyGraphic: true, groupCheckboxes: true },
     DynamicData: 
@@ -14,6 +14,24 @@ export default {
             visibleByDefault: true,
             layerOptions: {
                 layers: 'buildings_and_land:licensed_hmos',
+                popup: {
+                    icon: 'fa fa-home',
+                    body: {
+                      'Address': 'address',
+                      'Licence Ref': 'uni_ref',
+                      'Licence Status': 'licence_status',
+                      'Data Source': 'data_source'
+                    }
+                  },
+            },
+        },
+        {
+            key: 'Unlicensed HMOs',
+            group: 'Houses in Multiple Occupation',
+            url: 'wms',
+            visibleByDefault: true,
+            layerOptions: {
+                layers: 'buildings_and_land:unlicensed_hmos',
                 popup: {
                     icon: 'fa fa-home',
                     body: {
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 const { DefinePlugin } = require('webpack')
 
-const solution = 'mobility'
+const solution = 'housing'
 
 module.exports = (env, argv, t) => (
     {
